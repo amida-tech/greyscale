8b90b6fe9478723ac0b73e62765a408894e3bc5c
Babushkin Semyon <babushkin.semyon@gmail.com>
2015-11-16 17:27:44 +0300

    run app using child process

--- client/app/greyscale.core/scripts/greyscale.core.js
+++ client/app/greyscale.core/scripts/greyscale.core.js
@@ -1,7 +1,6 @@
 /**
  * Created by igi on 10.11.15.
  */
- 'use strict';
 angular.module('greyscale.core', ['restangular'])
     .config(function (RestangularProvider) {
         RestangularProvider.setBaseUrl("http://indaba.ntrlab.ru:83/v0.2");
