624efd29e46b757194cb25b6b22ada29c298d647
Igor Ivanov <iivanov@sinergo.ru>
2015-12-26 16:51:23 +0600

    fixed cached API call issue

--- client/app/greyscale.rest/scripts/services/greyscale-organization.js
+++ client/app/greyscale.rest/scripts/services/greyscale-organization.js
@@ -6,18 +6,20 @@
 angular.module('greyscale.rest')
     .factory('greyscaleOrganizationSrv', function (greyscaleRestSrv) {
 
-        var _api= greyscaleRestSrv().one('organizations');
-
         return {
             list: _list,
             get: _get
         };
 
+        function api(){
+            return greyscaleRestSrv().one('organizations');
+        }
+
         function _list (param) {
-            return _api.get(param);
+            return api().get(param);
         }
 
         function _get (id) {
-            return _api.one(id).get();
+            return api().one(id).get();
         }
     });
