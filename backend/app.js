16717cda7fda339c4abbf85b4e23ef99a772b1b9
Jacob Sachs <jacob.s.sachs@gmail.com>
2016-01-12 14:05:21 -0500

    correct issue #8

--- backend/app.js
+++ backend/app.js
@@ -4,25 +4,16 @@
 
 var spawn = require('child_process').spawn,
   _ = require('underscore'),
-  test = (process.argv.length > 2 && process.argv[2] == 'test'),
   args = [],
   stdio = [];
 
-if (test) {
-  args = ['test/bootstrap.js'];
-  stdio = [process.stdin, process.stdout, process.stderr];
-} else {
-  args = _.union(process.execArgv, ['app/bootstrap.js']);
-  stdio = [process.stdin, process.stdout, process.stderr];
-}
+args = _.union(process.execArgv, ['app/bootstrap.js']);
+stdio = [process.stdin, process.stdout, process.stderr];
 
 var opt = {
   cwd: __dirname,
   env: (function () {
     process.env.NODE_PATH = '.'; // Enables require() calls relative to the cwd
-    if (test) {
-      process.env.NODE_ENV = 'test';
-    }
     return process.env;
   }()),
   stdio: stdio
