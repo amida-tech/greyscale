9563a502846abf1d7f63d5756cf56b76eb28eb5a
Igor Ivanov <iivanov@sinergo.ru>
2016-01-18 16:07:31 +0600

    missed module

---
+++ client/app/greyscale.core/scripts/services/greyscale-roles.js
@@ -0,0 +1,32 @@
+/**
+ * Created by igi on 18.01.16.
+ */
+
+'use strict';
+
+angular.module('greyscale.core')
+    .service('greyscaleRolesSrv', function ($q, greyscaleRoleSrv) {
+        var _roles = null;
+        var _rolesPromise = null;
+        this.getRoles = function (force) {
+            var res;
+
+            force = !!force;
+            if (!_roles || force) {
+                if (!_rolesPromise) {
+                    _rolesPromise = greyscaleRoleSrv.list()
+                        .then(function (data) {
+                            _roles = data;
+                            return _roles;
+                        })
+                        .finally(function () {
+                            _rolesPromise = null;
+                        });
+                }
+                res = _rolesPromise;
+            } else {
+                res = $q.resolve(_roles);
+            }
+            return res;
+        };
+    });
