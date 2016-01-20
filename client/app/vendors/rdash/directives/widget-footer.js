520b191f84872f86a266cc3d2487d443d6087a7c
Igor Ivanov <iivanov@sinergo.ru>
2016-01-20 19:38:17 +0600

    re-factored RDash widgets, added users-uoa page, added checkbox-list directive, fixed typo

--- client/app/vendors/rdash/directives/widget-footer.js
+++ client/app/vendors/rdash/directives/widget-footer.js
@@ -1,17 +1,17 @@
 /**
  * Widget Footer Directive
  */
+(function () {
+    'use strict';
+    angular.module('RDash')
+        .directive('rdWidgetFooter', rdWidgetFooter);
 
-angular
-    .module('RDash')
-    .directive('rdWidgetFooter', rdWidgetFooter);
-
-function rdWidgetFooter() {
-    var directive = {
-        requires: '^rdWidget',
-        transclude: true,
-        template: '<div class="widget-footer" ng-transclude></div>',
-        restrict: 'E'
-    };
-    return directive;
-};
+    function rdWidgetFooter() {
+        return {
+            requires: '^rdWidget',
+            transclude: true,
+            template: '<div class="widget-footer" ng-transclude></div>',
+            restrict: 'E'
+        };
+    }
+})();
