a7526398deb85f61148fd3ae6da5eadeb6b480d6
Jacob Sachs <jacob.s.sachs@gmail.com>
2016-01-21 14:22:30 -0500

    GREY-28 additional graph views
    Add scaffolding for inserting graph directives, a la the map
    visualization view.

---
+++ client/app/scripts/controllers/graph.js
@@ -0,0 +1,18 @@
+/**
+ * Created by jsachs on 21.01.16
+ *
+ * @ngdoc function
+ * @name greyscaleApp.controller:GraphCtrl
+ * @description
+ * # GraphCtrl
+ * Controller of the greyscaleApp
+ */
+
+'use strict';
+
+angular.module('greyscaleApp').controller('GraphCtrl', function ($scope) {
+
+    $scope.model = {
+
+    };
+});
