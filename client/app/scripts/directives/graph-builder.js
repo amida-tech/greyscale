38f96cebf60ad03e97b8f1dad8cc4b393acc9794
Jacob Sachs <jacob.s.sachs@gmail.com>
2016-01-24 23:23:21 -0500

    GREY-26 variable selection
    Y-variable selection has been split into two options:
    - A histogram, with the x-val representing buckets
    - We still use x-values as buckets, with the selected y-value averaged
    for each x entry.

--- client/app/scripts/directives/graph-builder.js
+++ client/app/scripts/directives/graph-builder.js
@@ -21,12 +21,12 @@ angular
                  * Data variables
                  */
                 scope.data = [
-                    { 'country': 'United States', 'value': 'B' },
-                    { 'country': 'Canada', 'value': 'B' },
-                    { 'country': 'Australia', 'value': 'B' },
-                    { 'country': 'United Kingdom', 'value': 'A' },
-                    { 'country': 'Mexico', 'value': 'C' },
-                    { 'country': 'Argentina', 'value': 'C' }               
+                    { 'country': 'United States', 'value': 'B', 'score': 20 },
+                    { 'country': 'Canada', 'value': 'B', 'score': 21 },
+                    { 'country': 'Australia', 'value': 'B', 'score': 22 },
+                    { 'country': 'United Kingdom', 'value': 'A', 'score': 23 },
+                    { 'country': 'Mexico', 'value': 'C', 'score': 24 },
+                    { 'country': 'Argentina', 'value': 'C', 'score': 25 }               
                 ];
                 
                 /*
@@ -37,7 +37,7 @@ angular
                 });
 
                 /*
-                 * Plotly functions
+                 * Plotly data generation functions
                  */
                 var layout = {
                     autosize: true,
@@ -55,6 +55,28 @@ angular
                 function generateData(data, xVal, yVal) {
                     var x = [];
                     var y = [];
+                    
+                    // Group the data into x-axis value buckets
+                    var groups = _.groupBy(data, xVal);
+                    
+                    // The score for each bucket becomes the avage of the y-values
+                    _.forEach(groups, function (g, key) {
+                        x.push(key);
+                        var tot = 0
+                        _.forEach(g, function(val) {
+                            tot += val[yVal];
+                        });
+                        y.push(tot / g.length);
+                    });
+                    
+                    // Sort the arrays dependently,
+                    // and return in the Plotly data format.
+                    return sortAndPackData(x, y);
+                }
+                
+                function generateDataHistogram(data, xVal) {
+                    var x = [];
+                    var y = [];
 
                     // Group the data into x-axis value buckets
                     var groups = _.groupBy(data, xVal);
@@ -65,24 +87,47 @@ angular
                         y.push(g.length);
                     });
                         
-                    // Sort the arrays dependently
+                    // Sort the arrays dependently,
+                    // and return in the Plotly data format.
+                    return sortAndPackData(x, y);
+                }
+                
+                function sortAndPackData(x, y) {
+                    var sorted = sortArraysDependent(x, y);           
+                    return packData(sorted);
+                }
+                
+                function sortArraysDependent(x, y) {
                     var zipped = _.zip(x, y);
                     var sorted = _.sortBy(zipped, function (a) {
                         return a[0];
                     });
                     var unzipped = _.unzip(sorted);
-
+                    return [unzipped[0], unzipped[1]];
+                }
+                
+                function packData(sortedData) {
                     return [{
-                        'x': unzipped[0],
-                        'y': unzipped[1],
+                        'x': sortedData[0],
+                        'y': sortedData[1],
                     }];
                 }
                 
-                scope.drawGraph = function (graphType) {
+                scope.drawGraph = function () {
+                    
+                    // format the data for the type of graph being used
+                    var graphData;
+                    switch (scope.graphY) {
+                        case "Count":
+                            graphData = generateDataHistogram(scope.data, scope.graphX);
+                            break;
+                        default:
+                            graphData = generateData(scope.data, scope.graphX, scope.graphY);
+                            break;
+                    }
 
-                    var graphData = generateData(scope.data, scope.graphX, scope.graphY);
-
-                    switch (graphType) {
+                    // add Plotly attributed for the type of graph being used
+                    switch (scope.graphType) {
                         case "Scatter":
                             graphData[0].type = 'scatter';
                             graphData[0].mode = 'markers';
@@ -97,7 +142,7 @@ angular
                             graphData[0].type = 'bar';
                             break;
                     }
-
+                    console.log(graphData);
                     Plotly.newPlot('viz', graphData, layout);
                 };
 
