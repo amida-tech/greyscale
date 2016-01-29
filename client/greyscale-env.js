e92cdd364484074256fc7f86232969d9a25527d3
mhiner <mike.hiner@gmail.com>
2016-01-29 15:44:39 +0000

    remove extra : from calculated URL

--- client/greyscale-env.js
+++ client/greyscale-env.js
@@ -2,7 +2,7 @@ angular
     .module('greyscale.core')
     .constant('greyscaleEnv', {
         name:'docker',
-        apiProtocol: window.location.protocol,
+        apiProtocol: window.location.protocol.substring(0,window.location.protocol.length-1),
         apiHostname: window.location.host,
         apiRealm: 'api/' + window.location.hostname.split('.')[0],
         apiVersion: 'v0.2',
