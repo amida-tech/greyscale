d4a1e018207b2c4a48ce0cf7b3a49b40bd83a7a9
Jacob Sachs <jacob.s.sachs@gmail.com>
2016-02-01 14:25:19 -0500

    re-add base config file

---
+++ backend/config_etalon.js
@@ -0,0 +1,88 @@
+var environments = {
+  development: {
+    port: 3005, // Port for incoming HTTP. Can be overriden by env. variable PORT
+    encoding: 'utf8',
+    domain: 'http://localhost:3005',
+    authToken: {
+      expiresAfterSeconds: 360000 * 24 // 24 hour
+    },
+    pgConnect: {
+      user: process.env.RDS_USERNAME     || process.env.INDABA_PG_USERNAME || 'semyon',
+      password: process.env.RDS_PASSWORD || process.env.INDABA_PG_PASSWORD || 'aw34res',
+      database: process.env.INDABA_PG_DB || 'indaba',
+      host: process.env.RDS_HOSTNAME     || process.env.INDABA_PG_HOSTNAME || 'localhost',
+      port: process.env.INDABA_PG_PORT   || 5432
+    },
+    admin_role: 'admin',
+    auth: {
+      salt: process.env.AUTH_SALT || 'nMsDo)_1fh'
+    },
+    allowedDomains: '*', // for CORS
+    email: {
+      transport: {
+        opts: {
+          host: process.env.MAIL_HOST || 'smtp.gmail.com',
+          port: process.env.MAIL_PORT || 465,
+          auth: {
+            user: process.env.MAIL_USER || 'indaba.msk2015@gmail.com',
+            pass: process.env.MAIL_PASS || 'indabamsk2015'
+          },
+          secure: true
+        }
+      },
+      sender: {
+        name: "Indaba", // TODO
+        email: "hello@indaba.com"
+      }
+    }
+  },
+  test: {
+    port: 3005, // Port for incoming HTTP. Can be overriden by env. variable PORT
+    encoding: 'utf8',
+    domain: 'http://localhost:3005',
+    authToken: {
+      expiresAfterSeconds: 360000 * 24 // 24 hour
+    },
+    logging: {
+        transport: 'console',
+        options: {
+            level: 'error',
+            colorize: true,
+            timestamp: true
+        }  
+    },
+    pgConnect: {
+      user: 'postgres',
+      password: '',
+      database: 'indabatest',
+      host: 'localhost',
+      port: 5432
+    },
+    admin_role: 'admin',
+    auth: {
+      salt: process.env.AUTH_SALT || 'nMsDo)_1fh'
+    },
+    allowedDomains: '*', // for CORS
+    email: {
+      transport: {
+        opts: {
+          host: process.env.MAIL_HOST || 'smtp.gmail.com',
+          port: process.env.MAIL_PORT || 465,
+          auth: {
+            user: process.env.MAIL_USER || 'indaba.msk2015@gmail.com',
+            pass: process.env.MAIL_PASS || 'indabamsk2015'
+          },
+          secure: true
+        }
+      },
+      sender: {
+        name: "Indaba", // TODO
+        email: "hello@indaba.com"
+      }
+    }
+  }
+};
+
+// Take configuration according to environment
+var nodeEnv = process.env.NODE_ENV || 'development';
+module.exports = environments[nodeEnv] || environments.development;
