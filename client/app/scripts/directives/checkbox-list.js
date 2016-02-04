1bd81081f8e20fdb09c7a6c02130c95a2107b441
Igor Ivanov <iivanov@sinergo.ru>
2016-02-04 15:27:30 +0600

    re-styled checkboxes, refactored form-builder directive

--- client/app/scripts/directives/checkbox-list.js
+++ client/app/scripts/directives/checkbox-list.js
@@ -8,7 +8,7 @@ angular.module('greyscaleApp')
         return {
             restrict: 'E',
             template: '<div class="panel panel-default"> <div class="checkbox" ng-repeat="item in listItems" ' +
-                'ng-show="listItems.length>0"><label><input type="checkbox" ng-model="item.checked" ng-change="onItemChange(item)">{{item.name}}</label></div>' +
+                'ng-show="listItems.length>0"><label><input type="checkbox"  disabled="disabled" ng-model="item.checked" ng-change="onItemChange(item)"><div class="chk-box"></div>{{item.name}}</label></div>' +
                 '<div class="row text-center" ng-hide="listItems.length>0"><h5>No Data.</h5></div></div>',
             scope: {
                 listItems: '=?',
