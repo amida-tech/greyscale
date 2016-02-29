b992f4ba1f6bfcb92074f8484d0ace70d475ab13
mhiner <mike.hiner@gmail.com>
2016-02-29 21:48:07 +0000

    setting realm to a hardcoded public

--- client/greyscale-env.js
+++ client/greyscale-env.js
@@ -4,7 +4,7 @@ angular
         name:'docker',
         apiProtocol: window.location.protocol.substring(0,window.location.protocol.length-1),
         apiHostname: window.location.host,
-        apiRealm: 'api/' + window.location.hostname.split('.')[0],
+        apiRealm: 'api/public',
         apiVersion: 'v0.2',
         defaultUser:'no@mail.net',
         defaultPassword:'testuser',
