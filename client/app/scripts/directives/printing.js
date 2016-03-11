806c43b6e9dc62e1796f2573bf58e1a14c800ac1
alexcoach <lexa.trener@gmail.com>
2016-03-11 18:00:01 +0300

    survey printing

---
+++ client/app/scripts/directives/printing.js
@@ -0,0 +1,30 @@
+'use strict';
+
+angular.module('greyscaleApp')
+.directive('printArea', function(){
+    return {
+        restrict: 'A',
+        link: function(scope, el, attr){
+            var printable = $('#printable');
+            if (!printable.length) {
+                printable = $('<div id="printable"></div>')
+                $('body').append(printable);
+            }
+
+            el.on('click', function(){
+                var id = attr.printArea;
+                var area = $('[printing-area="' + id + '"]');
+                if (area.length) {
+                    _printArea(area);
+                }
+            });
+
+            function _printArea(area) {
+                var mode = attr.printMode || '';
+                var printing = $('<div><div class="' + mode + '">' + area.clone().html() + '</div></div>');
+                printable.html(printing.html());
+                window.print();
+            }
+        }
+    };
+});
