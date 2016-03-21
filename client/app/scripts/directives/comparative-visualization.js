259afc977b9e7fa2cb478dd55de475aa8eacb9a6
Harry Rickards <rickards@mit.edu>
2016-03-21 16:47:45 +0100

    Comparative viz color scale/dynamic update/tweaks

--- client/app/scripts/directives/comparative-visualization.js
+++ client/app/scripts/directives/comparative-visualization.js
@@ -77,8 +77,10 @@ angular.module('greyscaleApp')
                     });
 
                     $q.all(promises).then(function () {
-                        var data = preprocessData(products);
-                        renderVisualization(data);
+                        renderVisualization(preprocessData(products));
+                        if (products.length === 0) {
+                            clearVisualization();
+                        }
                     });
                 }, true);
 
@@ -86,18 +88,27 @@ angular.module('greyscaleApp')
                 // VISUALIZATION
                 var layout = {
                     targets: {
-                        hPadding: 10,
-                        vPadding: 10,
-                        width: 25,
-                        height: 25
+                        hPadding: 12.5,
+                        vPadding: 12.5,
+                        width: 35,
+                        height: 35
                     },
                     targetLabels: {
-                        fontSize: 10,
-                        height: 50
+                        fontSize: 12,
+                        height: 75,
+                        vPadding: 5
                     },
                     productLabels: {
-                        fontSize: 10
-                    }
+                        fontSize: 12,
+                        hPadding: 5,
+                        width: 125
+                    },
+                    axis: {
+                        colorWidth: 20,
+                        innerPadding: 5,
+                        minHeight: 150
+                    },
+                    colors: ['white', 'blue'] // 2 only
                 };
 
                 function preprocessData(productIndexes) {
@@ -128,8 +139,15 @@ angular.module('greyscaleApp')
                     return data;
                 }
 
+                function clearVisualization(data) {
+                    var d3 = $window.d3;
+                    var svg = d3.select('svg');
+                    svg.select('#grid #background').attr('fill', '#fff');
+                    svg.select('#scale #axis').selectAll('*').remove();
+                    svg.select('#scale rect').attr('fill', '#fff');
+                }
+
                 function renderVisualization(data) {
-                    console.log(data);
                     var d3 = $window.d3;
 
                     // construct color scale
@@ -140,70 +158,126 @@ angular.module('greyscaleApp')
                     }));
                     var color = d3.scale.linear()
                         .domain([_.min(vals), _.max(vals)])
-                        .range(['white', 'blue']);
+                        .range(layout.colors);
+
+                    var svg = d3.select('svg');
 
                     // hover text
-                    /*var tip = d3.tip().attr('class', 'd3-tip').html('asd');
-                      svg.call(tip);*/
+                    // TODO: fix tooltips (not working with dynamic data viz)
+                    /*var tip = d3.tip()
+                        .attr('class', 'd3-tip')
+                        .html(function (d) { return d.val; });
+                    svg.call(tip);*/
 
                     // grid of targets
                     var l = layout.targets;
-                    var svg = d3.select('svg');
-                    var grid = svg.append('g')
-                        .attr('transform', 'translate(0, ' + layout.targetLabels.height + ')');
+                    var grid = svg.select('#grid')
+                        .attr('transform', 'translate(0, ' + (layout.targetLabels.height + layout.targetLabels.vPadding) + ')');
 
                     // background so white ===> min index value, bg ===> value not present
                     var gridWidth = l.targets.length * (l.width + l.hPadding) + l.hPadding;
                     var gridHeight = data.length * (l.height + l.vPadding) + l.vPadding;
-                    grid.append('rect')
+                    grid.select('#background')
                         .attr('width', gridWidth)
                         .attr('height', gridHeight)
                         .attr('fill', '#eee');
 
-                    var rows = grid.selectAll('.row').data(data).enter()
-                        .append('g')
+                    var rows = grid.selectAll('.row').data(data);
+                    rows.enter().append('g')
                         .attr('class', 'row');
+                    rows.exit().remove();
 
-                    var cols = rows.selectAll('.target')
-                        .data(function (d) { return d.data; })
-                        .enter().append('rect')
+                    var cols = rows.selectAll('.target').data(function (d) { return d.data; });
+                    cols.enter().append('rect')
                         .attr('class', 'target')
-                        .attr('x', function (d) { return d.x; })
-                        .attr('y', function (d) { return d.y; })
                         .attr('width', l.width)
                         .attr('height', l.height)
+                        /*.on('mouseover', tip.show)
+                        .on('mouseout', tip.hide)*/;
+                    cols.exit().remove();
+                    cols
+                        .attr('x', function (d) { return d.x; })
+                        .attr('y', function (d) { return d.y; })
                         .style('fill', function (d) { return color(d.val); });
 
                     // target labels
-                    var targetLabels = svg.append('svg');
+                    var targetLabels = svg.select('#targetLabels');
                     l = layout.targetLabels;
                     var offset = layout.targets.hPadding + layout.targets.width / 2 - l.fontSize / 2;
-                    targetLabels.selectAll('.label')
-                        .data(layout.targets.targets)
-                        .enter().append('text')
+                    var labels = targetLabels.selectAll('.label').data(layout.targets.targets);
+                    labels.enter().append('text')
                         .attr('class', 'label')
                         .attr('text-anchor', 'start')
-                        .attr('y', function (d) { return layout.targets.positions[d.id] + offset; })
                         .attr('x', -l.height)
                         .attr('transform', 'rotate(-90)')
-                        .style('font-size', l.fontSize + 'px')
+                        .style('font-size', l.fontSize + 'px');
+                    labels.exit().remove();
+                    labels
+                        .attr('y', function (d) { return layout.targets.positions[d.id] + offset; })
                         .text(function (d) { return d.name; });
 
                     // product labels
                     l = layout.productLabels;
+                    var hOffset = gridWidth + l.hPadding;
+                    var vizWidth = hOffset + l.width + l.hPadding;
                     var vHeight = layout.targets.vPadding + layout.targets.height;
-                    var vOffset = layout.targets.vPadding + layout.targets.height / 2 + l.fontSize / 2;
-                    var productLabels = svg.append('g')
-                        .attr('transform', 'translate(' + gridWidth + ', ' + layout.targetLabels.height + ')');
-                    productLabels.selectAll('.label')
-                        .data(data)
-                        .enter().append('text')
+                    offset = layout.targets.vPadding + layout.targets.height / 2 + l.fontSize / 2;
+                    var vOffset = layout.targetLabels.height + layout.targetLabels.vPadding;
+                    var vizHeight = vOffset + data.length * vHeight + layout.targets.vPadding;
+                    var productLabels = svg.select('#productLabels')
+                        .attr('transform', 'translate(' + hOffset + ', ' + vOffset + ')');
+                    labels = productLabels.selectAll('.label').data(data);
+                    labels.enter().append('text')
                         .attr('class', 'label')
                         .attr('x', 0)
-                        .attr('y', function (d, i) { return i * vHeight + vOffset; })
-                        .style('font-size', l.fontSize + 'px')
+                        .style('font-size', l.fontSize + 'px');
+                    labels.exit().remove();
+                    labels
+                        .attr('y', function (d, i) { return i * vHeight + offset; })
                         .text(function (d) { return d.product.title; });
                     
+
+                    // color scale axis
+                    l = layout.axis;
+                    var axisHeight = vizHeight - vOffset;
+                    if (axisHeight < l.minHeight) { axisHeight = l.minHeight; }
+                    var scale = svg.select('#scale')
+                        .attr('transform', 'translate(' + vizWidth + ', ' + vOffset + ')');
+                    var pos = d3.scale.linear() // position encoder
+                        .domain(color.domain())
+                        .range([0, axisHeight]);
+                    var axis = d3.svg.axis()
+                        .scale(pos)
+                        .orient('right')
+                        .ticks(5);
+
+                    // colored scale
+                    var gradient = svg.select('defs')
+                        .append('linearGradient')
+                        .attr('id', 'scaleGradient')
+                        .attr('x1', '0%')
+                        .attr('y1', '0%')
+                        .attr('x2', '0%')
+                        .attr('y2', '100%');
+                    gradient.append('stop')
+                        .attr('offset', '0%')
+                        .attr('stop-color', layout.colors[0])
+                        .attr('stop-opacity', 1);
+                    gradient.append('stop')
+                        .attr('offset', '100%')
+                        .attr('stop-color', layout.colors[1])
+                        .attr('stop-opacity', 1);
+                    scale.select('rect')
+                        .attr('height', axisHeight)
+                        .attr('width', l.colorWidth)
+                        .attr('fill', 'url(#scaleGradient)');
+
+                    // numerical axis
+                    var axisG = scale.select('#axis')
+                        .attr('transform', 'translate(' + (l.colorWidth + l.innerPadding) + ', 0)')
+                        .attr('class', 'axis')
+                        .call(axis);
+
 
                 }
             }
