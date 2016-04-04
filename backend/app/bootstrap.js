1fbb5e85d10ef86fac61836971a5db30d2989448
Harry Rickards <rickards@mit.edu>
2016-04-04 19:36:59 +0200

    Fix file upload crash bug

--- backend/app/bootstrap.js
+++ backend/app/bootstrap.js
@@ -87,6 +87,10 @@ app.on('start', function () {
         });
     });
 
+    // multipart form-data
+    // keep above logger to avoid obscure crashes
+    app.use(multer());
+
     // Init logger
     app.use(logger.initialize());
 
@@ -98,9 +102,6 @@ app.on('start', function () {
 
     // Parse JSON requests using body-parser
     app.use(bodyParser.json());
-
-    // form-data
-    app.use(multer());
 
     // view engine
     app.engine('ejs', require('ejs-locals'));
