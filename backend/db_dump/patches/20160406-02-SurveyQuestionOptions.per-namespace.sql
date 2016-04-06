2d4a25b04b3950e7f71335c9ff504b98dc8ab198
d.tseytlin <dtseytlin@gmail.com>
2016-04-06 22:01:13 +0300

    change constraint for SurveyQuestionOptions Run this script for each namespace in DB.

---
+++ backend/db_dump/patches/20160406-02-SurveyQuestionOptions.per-namespace.sql
@@ -0,0 +1,14 @@
+DO $$
+DECLARE  qry              text;
+BEGIN
+  RAISE NOTICE 'Current schema: %', current_schema;
+
+--  change FK constraint
+  qry := 'ALTER TABLE ' || quote_ident(current_schema) || '."SurveyQuestionOptions"'
+                          || ' DROP CONSTRAINT "surveyQuestionOptions_questionId_fkey", ' 
+                          || ' ADD CONSTRAINT "surveyQuestionOptions_questionId_fkey" ' 
+                          || 'FOREIGN KEY ("questionId") REFERENCES '|| quote_ident(current_schema) ||'."SurveyQuestions" ("id") ON DELETE CASCADE ON UPDATE CASCADE;';
+  RAISE NOTICE '%', qry;
+  --EXECUTE qry;
+
+END $$;
