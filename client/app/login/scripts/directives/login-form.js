84740a304f8fe25ced5f23e6bb0c3cc67ddb0150
Igor Ivanov <iivanov@sinergo.ru>
2016-04-11 18:22:05 +0600

    separate login

---
+++ client/app/login/scripts/directives/login-form.js
@@ -0,0 +1,45 @@
+'use strict';
+
+angular.module('greyscaleApp')
+    .directive('loginForm', function (greyscaleUserApi, greyscaleEnv) {
+        return {
+            templateUrl: 'views/directives/login-form.html',
+            restrict: 'AE',
+            scope: {},
+            controller: function ($scope, $rootScope) {
+                $scope.model = {
+                    login: greyscaleEnv.defaultUser || '',
+                    password: greyscaleEnv.defaultPassword || '',
+                    realms: [],
+                    error: null
+                };
+
+                $scope.loginChange = initForm;
+
+                $scope.submitLogin = function () {
+                    if ($scope.loginForm.$valid) {
+                        greyscaleUserApi.login($scope.model.login, $scope.model.password)
+                            .then(function () {
+                                $rootScope.$emit('login');
+                            })
+                            .catch(function (err) {
+                                if (err && err.data && err.data.e === 300) {
+                                    $scope.model.realms = err.data.message;
+                                } else {
+                                    $scope.model.error = 'LOGIN.CHECK_EMAIL_PASSWORD';
+                                }
+                            });
+                    } else {
+                        $scope.model.error = 'LOGIN.CHECK_LOGIN_PASSWORD';
+                    }
+                };
+
+                function initForm() {
+                    angular.extend($scope.model, {
+                        realms: [],
+                        error: null
+                    });
+                }
+            }
+        };
+    });
---
+++ client/app/login/scripts/login.js
@@ -0,0 +1,3 @@
+/**
+ * Created by igi on 11.04.16.
+ */
