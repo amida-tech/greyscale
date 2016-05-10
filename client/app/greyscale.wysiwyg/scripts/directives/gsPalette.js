7c6672e090362525365cec809b6626c84b8388ec
Igor Ivanov <iivanov@sinergo.ru>
2016-05-10 21:23:56 +0600

    draft palette generator

--- client/app/greyscale.wysiwyg/scripts/directives/gsPalette.js
+++ client/app/greyscale.wysiwyg/scripts/directives/gsPalette.js
@@ -14,23 +14,35 @@ angular.module('greyscale.wysiwyg')
                     cols: 8
                 };
 
-                var r, c, row, _color, rgb;
+                var r, c, subColor, _color, rgb;
 
-                $scope.model.palette =[];
+                $scope.model.palette = [];
 
                 for (r = 0; r < $scope.model.rows; r++) {
                     $scope.model.palette.push([]);
                     for (c = 0; c < $scope.model.cols; c++) {
-                        _color = c+ r*$scope.model.cols;
-                        rgb = [
-                            ((_color & 0x30) << 2).toString(16),
-                            ((_color & 0xC) << 4).toString(16),
-                            ((_color & 0x3) << 6).toString(16)
-                        ];
-                        $log.debug(_color.toString(16), rgb);
-                        $scope.model.palette[r].push('#'+rgb.join(''));
+                        _color = c + r * $scope.model.cols;
+
+                        rgb = [];
+                        subColor = (_color & 0x30);
+                        rgb.push(toHexByte(subColor << 2 | subColor | subColor >> 2 | subColor >> 4));
+                        subColor = (_color & 0xC);
+                        rgb.push(toHexByte(subColor << 4 | subColor << 2 | subColor | subColor >> 2));
+                        subColor = (_color & 0x3);
+                        rgb.push(toHexByte(subColor << 6 | subColor << 4 | subColor << 2 | subColor));
+
+                        $log.debug(_color.toString(16,2), rgb);
+                        $scope.model.palette[r].push('#' + rgb.join(''));
                     }
                 }
             }
         };
+
+        function toHexByte(intVal) {
+            var res = intVal.toString(16);
+            if (res.length < 2) {
+                res = '0'+res;
+            }
+            return res;
+        }
     });
