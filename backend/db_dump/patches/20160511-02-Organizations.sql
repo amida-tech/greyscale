a9c26292dc5d8039db64967de87f3372ebea2c4a
Babushkin Semyon <babushkin.semyon@gmail.com>
2016-05-11 12:25:49 +0300

    issue 293

---
+++ backend/db_dump/patches/20160511-02-Organizations.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE FUNCTION patch_20160511_02_organizations() RETURNS void AS
+$BODY$
+DECLARE
+    schema_name text;
+BEGIN
+	FOR schema_name IN
+		SELECT pg_catalog.pg_namespace.nspname
+		FROM pg_catalog.pg_namespace
+		INNER JOIN pg_catalog.pg_user
+		ON (pg_catalog.pg_namespace.nspowner = pg_catalog.pg_user.usesysid)
+		AND (pg_catalog.pg_user.usename = 'indaba') -- HAVE TO SET CORRECT DB USER
+	LOOP
+		EXECUTE 'SET search_path TO ' || quote_ident(schema_name);
+		EXECUTE 'ALTER TABLE "Organizations" ADD COLUMN "enableFeaturePolicy" boolean NOT NULL DEFAULT FALSE;';
+	END LOOP;
+
+END
+$BODY$
+LANGUAGE plpgsql;
+
+SELECT patch_20160511_02_organizations();
+DROP FUNCTION IF EXISTS public.patch_20160511_02_organizations();
+
