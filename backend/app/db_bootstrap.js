e654072333f08f34a1a52212c4ed31f8eefd9873
Babushkin Semyon <babushkin.semyon@gmail.com>
2016-05-17 16:56:55 +0300

    pg client end event

--- backend/app/db_bootstrap.js
+++ backend/app/db_bootstrap.js
@@ -2,13 +2,21 @@ var config = require('config'),
     logger = require('app/logger'),
     util = require('util'),
     Client = require('pg').Client;
+    debug = require('debug')('debug_db_bootstrap');
+    debug.log = console.log.bind(console);
 
 var ClientPG = function () {
 
     var client = new Client(config.pgConnect);
 
     client.on('error', function (err) {
-        logger.error(util.format('Connection error: %s', err));
+        debug(util.format('Connection error: %s', err));
+    });
+
+    client.on('drain', client.end.bind(client));
+
+    client.on('end', function(){
+        debug("Client was disconnected.");
     });
 
     return client;
