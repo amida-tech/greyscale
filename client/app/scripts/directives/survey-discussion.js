2d1f2bc467144a5db93ce8bd88e215e11947e782
Igor Ivanov <iivanov@sinergo.ru>
2016-05-18 19:31:15 +0600

    prevent user from flagging different WF-steps - subsequent attempts to create another flag will fallback target WF step (user) to the same that was created first

--- client/app/scripts/directives/survey-discussion.js
+++ client/app/scripts/directives/survey-discussion.js
@@ -27,7 +27,8 @@ angular.module('greyscaleApp')
                         list: []
                     },
                     associate: {},
-                    assignTo: []
+                    assignTo: [],
+                    draftFlag: null
                 };
 
                 $scope.surveyParams = {};
@@ -54,8 +55,13 @@ angular.module('greyscaleApp')
                 };
 
                 $scope.flagIsDisabled = function () {
-                    var hasUnresolvedFlag = false;
-                    angular.forEach($scope.model.questions, function (q) {
+                    var q, qQty, m, mQty,
+                        hasUnresolvedFlag = false;
+                    for (q = 0; q < qQty; q++) {
+                        if ($scope.model.questions.list[q]) {
+                        }
+                    }
+                    angular.forEach($scope.model.questions.list, function (q) {
                         if (q.messages && q.messages.length) {
                             angular.forEach(q.messages, function (m) {
                                 if (!hasUnresolvedFlag && m.flagged && !m.resolved) {
@@ -69,6 +75,18 @@ angular.module('greyscaleApp')
 
                 $scope.updateMsg = function (message) {
                     return greyscaleDiscussionApi.update(message.id, message);
+                };
+
+                $scope.flagChange = function () {
+                    if ($scope.model.msg.isReturn) {
+                        $scope.model.msg.stepId = $scope.model.draftFlag.stepId;
+                    } else {
+                        $scope.model.msg.stepId = null;
+                    }
+                };
+
+                $scope.filterSteps = function (elem) {
+                    return !$scope.model.msg.isReturn || !$scope.model.draftFlag || (elem.id === $scope.model.draftFlag.stepId);
                 };
 
                 $scope.removeMsg = function (message) {
@@ -106,19 +124,6 @@ angular.module('greyscaleApp')
                 }
             }
         };
-
-        function convertMsg(msg, associate) {
-            return {
-                id: msg.id,
-                from: msg.userFromId,
-                to: msg.userId,
-                sent: msg.created || new Date(),
-                flagged: msg.isReturn,
-                resolved: msg.isResolve,
-                draft: !msg.isActivated,
-                body: msg.entry
-            };
-        }
 
         function updateDicscussion(data, scope) {
 
@@ -189,6 +194,10 @@ angular.module('greyscaleApp')
                         if (scope.model.questions[msg.questionId]) {
                             scope.model.questions[msg.questionId].items.push(msg);
                         }
+                        if (!msg.activated && msg.isReturn && !msg.resolved) {
+                            scope.model.draftFlag = msg;
+                        }
+
                     }
 
                 });
