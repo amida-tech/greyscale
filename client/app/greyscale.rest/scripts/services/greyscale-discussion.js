8b488850194de1362b42cccb9bbcf787c39a469d
Igor Ivanov <iivanov@sinergo.ru>
2016-05-20 18:26:36 +0600

    discussion messages order

--- client/app/greyscale.rest/scripts/services/greyscale-discussion.js
+++ client/app/greyscale.rest/scripts/services/greyscale-discussion.js
@@ -4,7 +4,7 @@
 'use strict';
 
 angular.module('greyscale.rest')
-    .factory('greyscaleDiscussionApi', function (greyscaleRestSrv) {
+    .factory('greyscaleDiscussionApi', function (_, greyscaleRestSrv) {
         return {
             list: _list,
             scopeList: _scope,
@@ -28,7 +28,10 @@ angular.module('greyscale.rest')
         }
 
         function _list(params) {
-            return _api().get(params).then(_response);
+            params = angular.extend({order: 'created'}, params);
+            return _api().get(params).then(_response).then(function(data){
+                return data.reverse();
+            });
         }
 
         function _scope(params) {
