9fd76f85b7f670508e296fcbcadf4ed2cec53d77
alexcoach <lexa.trener@gmail.com>
2016-06-09 11:13:30 +0300

    add attachments to saving data

--- client/app/greyscale.rest/scripts/services/greyscale-attachment.js
+++ client/app/greyscale.rest/scripts/services/greyscale-attachment.js
@@ -5,11 +5,29 @@
 angular.module('greyscale.rest')
     .factory('greyscaleAttachmentApi', function (greyscaleRestSrv) {
         return {
-            delete: _delete
+            delete: _delete,
+            list: _list
         };
 
         function _api() {
             return greyscaleRestSrv().one('attachments');
+        }
+
+        function _preResp(resp) {
+            if (typeof resp.plain === 'function') {
+                return resp.plain();
+            } else {
+                return resp;
+            }
+        }
+
+        function _list(essenceId, entityId) {
+            return _api().get({
+                essenceId: essenceId,
+                entityId: entityId,
+                fields: 'id,filename,mimetype,size'
+            })
+            .then(_preResp);
         }
 
         function _delete(attachId) {
