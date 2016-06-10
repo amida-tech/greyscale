6f6635400b643d46be1fffdf54e623499a802db3
alexcoach <lexa.trener@gmail.com>
2016-06-10 17:18:58 +0300

    #345 fix radios and checkbox survey fields. fix include other in builder

--- client/app/scripts/directives/survey-form.js
+++ client/app/scripts/directives/survey-form.js
@@ -649,11 +649,12 @@ angular.module('greyscaleApp')
                         if (fld.withOther) {
                             fld.otherOption = {
                                 id: -1,
-                                value: answer.value || fld.value
+                                value: fld.value
                             };
 
-                            if (answer.value) {
+                            if (!answer.optionId.length) {
                                 fld.answer = fld.otherOption;
+                                fld.answer.value = answer.value;
                             }
                         }
 
@@ -777,8 +778,8 @@ angular.module('greyscaleApp')
                             }
                         }
 
-                        if (fld.withOther && fld.otherOption && fld.otherOption.checked) {
-                            answer.value = fld.otherOption.value;
+                        if (fld.withOther && fld.otherOption) {
+                            answer.value = fld.otherOption.checked ? fld.otherOption.value : '';
                         }
                         break;
 
