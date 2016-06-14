3d9ecff628a4c20d2c1467d9e5f80282ab713539
d.tseytlin <dtseytlin@gmail.com>
2016-06-14 13:01:26 +0300

    fix compare after jshint

--- backend/app/controllers/organizations.js
+++ backend/app/controllers/organizations.js
@@ -268,7 +268,7 @@ module.exports = {
                 throw new HttpError(403, 'Organization with id = ' + req.params.id + ' does not exist');
             }
 
-            if (req.user.roleID !== 1 && req.user.organizationId !== req.params.id) {
+            if (req.user.roleID !== 1 && req.user.organizationId !== parseInt(req.params.id)) {
                 throw new HttpError(
                     403,
                     'You cannot add user to other organizations'
