d02e0e024188bb73cbcc4736456d8583edbb4df0
d.tseytlin <dtseytlin@gmail.com>
2016-06-18 23:01:28 +0300

    tests correction

--- backend/test/testdb.sql
+++ backend/test/testdb.sql
@@ -5961,10 +5961,29 @@ LANGUAGE plpgsql;
 SELECT public.patch_20160603_01_attachmentLinks();
 DROP FUNCTION IF EXISTS public.patch_20160603_01_attachmentLinks();
 
+CREATE OR REPLACE FUNCTION public.patch_20160617-01-Tasks() RETURNS void AS
+$BODY$
+DECLARE
+    schema_name text;
+BEGIN
+	FOR schema_name IN
+		SELECT pg_catalog.pg_namespace.nspname
+		FROM pg_catalog.pg_namespace
+		INNER JOIN pg_catalog.pg_user
+		ON (pg_catalog.pg_namespace.nspowner = pg_catalog.pg_user.usesysid)
+		AND (pg_catalog.pg_user.usename = 'indaba') -- HAVE TO SET CORRECT DB USER
+	LOOP
+		EXECUTE 'SET search_path TO ' || quote_ident(schema_name);
+		EXECUTE
+            'ALTER TABLE "Tasks" ' ||
+            'DROP CONSTRAINT "Tasks_userId_fkey", ' ||
+            'ADD COLUMN "userIds" int4[], ' ||
+            'ADD COLUMN "groupIds" int[4]; ' ||
+            'UPDATE "Tasks" SET "userIds" = ARRAY["userId"]; ';
+	END LOOP;
+END
+$BODY$
+LANGUAGE plpgsql;
 
-
-
-
-
-
-
+SELECT public.patch_20160617-01-Tasks();
+DROP FUNCTION IF EXISTS public.patch_20160617-01-Tasks();
