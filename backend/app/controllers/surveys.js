5d9567491de1cfdfa628873cdf35c2a72088f8b6
d.tseytlin <dtseytlin@gmail.com>
2016-06-22 21:50:41 +0300

    import docx till END header

--- backend/app/controllers/surveys.js
+++ backend/app/controllers/surveys.js
@@ -75,16 +75,23 @@ module.exports = {
                     var $ = cheerio.load(html);
                     var obj = {};
 
-                    $('html').children().each(function (key, item) {
-                        if (item.name === 'h1') {
-                            var index = $(item).text().replace(new RegExp('[^a-zA-Z]', 'g'), '');
+                    var endOfDoc = 'END';
+
+                    $('html').children().each(function(key, item) {
+                        if (item.name == 'h1') {
+                            var index = $(item).text().replace(new RegExp('[^a-zA-Z]', 'g'), '').toUpperCase();
                             var current = item;
+                            debug('Item: '+$(item).html());
                             var content = '';
 
-                            while (['h1', 'table'].indexOf($(current).next()[0].name) === -1) {
-                                var nextItem = $(current).next()[0];
-                                content += $(nextItem).html();
-                                current = nextItem;
+                            if (index === endOfDoc) {
+                                return false;
+                            } else {
+                                while ($(current).next() && ['h1'].indexOf($(current).next()[0].name) == -1) {
+                                    var nextItem = $(current).next()[0];
+                                    content += $(nextItem).html();
+                                    current = nextItem;
+                                }
                             }
 
                             obj[index] = content;
