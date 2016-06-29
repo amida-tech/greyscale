ae0fc84731fab2b73d8250e9e3823ee7691c3a19
kopytov vladimir <vkopytov@sinergo.ru>
2016-06-29 18:27:29 +0300

    353 refactoring

--- client/app/scripts/controllers/pm-dashboard-product.js
+++ client/app/scripts/controllers/pm-dashboard-product.js
@@ -3,7 +3,7 @@
 angular.module('greyscaleApp')
     .controller('PmDashboardProductCtrl', function (_, $q, $scope, $state, $stateParams,
         greyscaleProductApi, greyscaleProductTasksTbl, $timeout, greyscaleUtilsSrv, greyscaleTokenSrv, greyscaleTaskApi, Organization, greyscaleModalsSrv,
-        greyscaleGlobals, greyscaleProductWorkflowApi, greyscaleSurveyApi, greyscaleProjectApi,  inform) {
+        greyscaleProjectProductsTbl, greyscaleSurveyApi) {
     
     var productId = $stateParams.productId;
     
@@ -23,9 +23,6 @@ angular.module('greyscaleApp')
         count: {}
     };
     
-    var _dicts = {
-        surveys: []
-    };
     greyscaleProductApi.get(productId).then(function (product) {
         $scope.model.product = product;
         
@@ -34,9 +31,7 @@ angular.module('greyscaleApp')
                 $scope.model.survey = survey;
             });
         }
-        greyscaleProjectApi.surveysList(product.projectId).then(function (surveys) {
-            _dicts.surveys = surveys;
-        });
+        greyscaleProjectProductsTbl.methods.fillSurvey(product.projectId);
         
         $state.ext.productName = product.title;
         return product;
@@ -66,8 +61,7 @@ angular.module('greyscaleApp')
         $scope.model.count.delayed = $scope.model.count.uoas - $scope.model.count.onTime;
     };
     
-    _getData(productId)
-            .then(function (data) {
+    _getData(productId).then(function (data) {
         $scope.model.tasks = data.tasks;
     });
     
@@ -90,166 +84,24 @@ angular.module('greyscaleApp')
     };
     
     $scope.editProductTasks = function () {
-        $state.go('projects.setup.tasks', {
-            productId: $scope.model.product.id,
-            product: $scope.model.product
-        });
-    };
-    
-    var tns = 'PRODUCTS.TABLE.';
-    
-    var _cols = [{
-            field: 'title',
-            title: tns + 'TITLE',
-            show: true,
-            dataRequired: true
-        }, {
-            field: 'description',
-            title: tns + 'DESCRIPTION',
-            show: true,
-            dataRequired: true,
-            dataFormat: 'textarea'
-        }, {
-            field: 'surveyId',
-            title: tns + 'SURVEY',
-            show: true,
-            dataFormat: 'option',
-            dataSet: {
-                getData: _getSurveys,
-                keyField: 'id',
-                valField: 'title',
-                groupBy: function (item) {
-                    return item.policyId ? 'Policies' : 'Surveys';
-                }
-            }
-        }, {
-            field: 'status',
-            show: true,
-            title: tns + 'STATUS',
-            dataFormat: 'option',
-            dataNoEmptyOption: true,
-            dataSet: {
-                getData: _getStatus,
-                keyField: 'id',
-                valField: 'name',
-                getDisabled: _getDisabledStatus
-            }
-        }];
-    
-    var _table = {
-        cols: _cols,
-        formTitle: tns + 'PRODUCT',
-    };
+        greyscaleProjectProductsTbl.methods.editProductTasks($scope.model.product);
+    }
     
     $scope.editProduct = function () {
-        var op = 'editing';
-        _loadProductExtendedData($scope.model.product).then(function (extendedProduct) {
-            var _editTable = angular.copy(_table);
-            return greyscaleModalsSrv.editRec(extendedProduct, _editTable);
-        }).then(function (newProduct) {
-            return greyscaleProductApi.update(newProduct);
-        }).then(function (product) {
-            $scope.model.product = product;
-        }).catch(function (err) {
-            return _errHandler(err, op);
+        greyscaleProjectProductsTbl.methods.editProduct($scope.model.product).then(function (product) {
+            if (product) $scope.model.product = product;
         });
-    };
+    }
     
     $scope.removeProduct = function () {
-        greyscaleModalsSrv.confirm({
-            message: 'PRODUCTS.TABLE.DELETE_CONFIRM',
-            product: $scope.model.product,
-            okType: 'danger',
-            okText: 'COMMON.DELETE'
-        }).then(function () {
-            greyscaleProductApi.delete($scope.model.product.id).then(function () {
-                $state.go('main');
-            }).catch(function (err) {
-                inform.add('Product delete error: ' + err);
-            });
+        greyscaleProjectProductsTbl.methods.removeProduct($scope.model.product).then(function () {
+            $state.go('main');
         });
     };
     
     $scope.editProductWorkflow = function () {
-        greyscaleModalsSrv.productWorkflow($scope.model.product).then(function (data) {
-            return _saveWorkflowAndSteps($scope.model.product, data);
-        }).then(function (product) {
-            $scope.model.product = product;
-        });
-    };
-    
-    function _saveWorkflowAndSteps(product, data) {
-        var promise = $q.when(data.workflow);
-        if (!_.isEqual(data.workflow, product.workflow)) {
-            promise = _saveProductWorkflow(data.workflow);
-        }
-        return promise.then(function (workflow) {
-            return _saveProductWorkflowSteps(workflow.id, data.steps);
-        });
-    }
-    
-    function _saveProductWorkflow(workflow) {
-        if (workflow.id) {
-            return greyscaleProductWorkflowApi.update(workflow).then(function () {
-                return $q.when(workflow);
-            });
-        } else {
-            return greyscaleProductWorkflowApi.add(workflow).then(function (response) {
-                workflow.id = response.id;
-                return $q.when(workflow);
-            });
-        }
-    }
-    
-    function _saveProductWorkflowSteps(workflowId, steps) {
-        return greyscaleProductWorkflowApi.workflow(workflowId).stepsListUpdate(steps);
-    }
-    
-    function _getSurveys() {
-        return _dicts.surveys;
-    }
-    
-    function _getStatus() {
-        return greyscaleGlobals.productStates;
-    }
-    
-    var _const = {
-        STATUS_PLANNING: 0,
-        STATUS_STARTED: 1,
-        STATUS_SUSPENDED: 2,
-        STATUS_CANCELLED: 4
-    };
-    function _getDisabledStatus(item, rec) {
-        return item.id !== _const.STATUS_PLANNING && item.id !== _const.STATUS_CANCELLED && _planningNotFinish(rec);
-    }
-    
-    function _planningNotFinish(product) {
-        return !product.uoas || !product.uoas.length || !product.surveyId ||
-                !product.workflowSteps || !product.workflowSteps.length || !product.tasks || !product.tasks.length;
-    }
-    
-    function _errHandler(err, operation) {
-        var msg = _table.formTitle + ' ' + operation + ' error';
-        greyscaleUtilsSrv.errorMsg(err, msg);
-    }
-    
-    function _loadProductExtendedData(product) {
-        if (!product || !product.id) {
-            return $q.when(product);
-        }
-        
-        var extendedProduct = angular.copy(product);
-        var reqs = {
-            uoas: greyscaleProductApi.product(product.id).uoasList(),
-            tasks: greyscaleProductApi.product(product.id).tasksList(),
-        };
-        if (product.workflow && product.workflow.id) {
-            reqs.workflowSteps = greyscaleProductWorkflowApi
-                    .workflow(product.workflow.id).stepsList();
-        }
-        return $q.all(reqs).then(function (promises) {
-            angular.extend(extendedProduct, promises);
-            return extendedProduct;
+        greyscaleProjectProductsTbl.methods.editProductWorkflow($scope.model.product).then(function (product) {
+            if (product) $scope.model.product = product;
         });
     }
     
@@ -286,5 +138,4 @@ angular.module('greyscaleApp')
         });
         return flaggedSurveys.length;
     }
-
 });
