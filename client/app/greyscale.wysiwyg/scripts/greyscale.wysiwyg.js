e0a8a615b0a31f2ba18121f239804ae27af6d2ff
Igor Ivanov <iivanov@sinergo.ru>
2016-07-20 18:01:09 +0700

    changed wysiwyg toolbar, changed red marker color code

--- client/app/greyscale.wysiwyg/scripts/greyscale.wysiwyg.js
+++ client/app/greyscale.wysiwyg/scripts/greyscale.wysiwyg.js
@@ -4,23 +4,10 @@
 'use strict';
 angular.module('greyscale.wysiwyg', ['textAngular'])
     .config(function ($provide) {
-        var _red = '#dd0000';
+        var _red = '#ff0000';
 
         $provide.decorator('taOptions', ['taRegisterTool', '$delegate', function (taRegisterTool, taOptions) {
-            /*
-             toolbar: [
-             ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre', 'quote'],
-             ['bold', 'italics', 'underline', 'strikeThrough', 'ul', 'ol', 'redo', 'undo', 'clear'],
-             ['justifyLeft','justifyCenter','justifyRight','justifyFull','indent','outdent'],
-             ['html', 'insertImage', 'insertLink', 'insertVideo', 'wordcount', 'charcount']
-             ],
-             */
-            taOptions.toolbar = [
-                ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p'],
-                ['bold', 'italics', 'underline', 'strikeThrough', 'ul', 'ol', 'redo', 'undo', 'clear']
-            ];
-
-            taRegisterTool('redColor', {
+            taRegisterTool('markRed', {
                 iconclass: 'fa fa-square text-danger',
                 tooltiptext: 'Mark red',
                 action: function () {
@@ -32,7 +19,21 @@ angular.module('greyscale.wysiwyg', ['textAngular'])
                 },
                 activeState: _isRed
             });
-            taOptions.toolbar[1].unshift('redColor');
+            /*
+             toolbar: [
+             ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre', 'quote'],
+             ['bold', 'italics', 'underline', 'strikeThrough', 'ul', 'ol', 'redo', 'undo', 'clear'],
+             ['justifyLeft','justifyCenter','justifyRight','justifyFull','indent','outdent'],
+             ['html', 'insertImage', 'insertLink', 'insertVideo', 'wordcount', 'charcount']
+             ],
+             */
+            taOptions.toolbar = [
+                ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
+                ['markRed', 'bold', 'italics', 'underline', 'strikeThrough'],
+                ['ul', 'ol'],
+                ['redo', 'undo', 'clear']
+            ];
+
             return taOptions;
 
             function _isRed(elem) {
@@ -42,7 +43,8 @@ angular.module('greyscale.wysiwyg', ['textAngular'])
                 }
                 if (elem) {
                     res = elem.attr('color') === _red ||
-                        elem.css('color') === 'rgb(221, 0, 0)';
+                        elem.attr('color') === 'red' ||
+                        elem.css('color') === 'rgb(255, 0, 0)';
                 }
                 return res;
             }
