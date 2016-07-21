fbefc3390a928dcefdee779efc347ceb222d3726
d.tseytlin <dtseytlin@gmail.com>
2016-07-21 12:44:43 +0300

    #GREY-307 possible delete/edit only draft entries (flagged)

--- backend/app/controllers/discussions.js
+++ backend/app/controllers/discussions.js
@@ -667,8 +667,10 @@ function* getAvailableUsers(req) {
 }
 
 function* checkNextEntry(req, id, checkOnly) {
-    var result;
+    //var result;
     var entry = yield * common.getDiscussionEntry(req, id);
+    return !entry.activated;
+/*
     var task = yield * common.getTask(req, entry.taskId);
     var productId = task.productId;
     var uoaId = task.uoaId;
@@ -689,9 +691,10 @@ function* checkNextEntry(req, id, checkOnly) {
         if (checkOnly) {
             return false;
         }
-        throw new HttpError(403, 'Entry with id=`' + id + '` cannot be updated or deleted, there are have following entries');
+        throw new HttpError(403, 'Entry with id=`' + id + '` cannot be updated or deleted as there are child entries exists');
     }
     return true;
+*/
 }
 
 function* getNextOrder(req, taskId, questionId) {
