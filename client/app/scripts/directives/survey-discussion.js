217e6ef7698952631b9e447545e4c87207706d38
Sean Bolak <seanbolak@gmail.com>
2016-07-22 17:46:00 -0400

    fixed client side flag delete

--- client/app/scripts/directives/survey-discussion.js
+++ client/app/scripts/directives/survey-discussion.js
@@ -5,7 +5,6 @@
 angular.module('greyscaleApp')
     .directive('surveyDiscussion', function (greyscaleGlobals, i18n, greyscaleDiscussionApi, greyscaleProfileSrv,
         greyscaleUtilsSrv, greyscaleProductWorkflowApi, _, $q) {
-        var fieldTypes = greyscaleGlobals.formBuilder.fieldTypes;
         var sectionTypes = greyscaleGlobals.formBuilder.excludedIndexes,
             flaggedQuestions = [],
             flaggedStep = null,
@@ -47,6 +46,7 @@ angular.module('greyscaleApp')
                         .then(function (resp) {
                             if ($scope.model.questions[body.questionId]) {
                                 angular.extend(body, resp);
+                                body.activated = !body.isReturn;
                                 $scope.model.questions[body.questionId].items.unshift(body);
                                 flaggedQuestions.push(body.questionId);
                             }
@@ -83,13 +83,9 @@ angular.module('greyscaleApp')
                 };
 
                 $scope.filterSteps = function (elem) {
-                    if (elem.id === $scope.surveyParams.currentStepId) {
-                        return false;
-                    }
-                    if ($scope.model.msg.isReturn && elem.position > currentStep.position) {
-                        return false;
-                    }
-                    return true;
+                    var res = (elem.id !== $scope.surveyParams.currentStepId);
+                    res =  res && (!$scope.model.msg.isReturn || elem.position <= currentStep.position);
+                    return res;
                 };
 
                 $scope.filterQuests = function (quest) {
