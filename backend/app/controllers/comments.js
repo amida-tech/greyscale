da1bb568a63b4d59ab2475a85a26b9fbe98e0bc2
d.tseytlin <dtseytlin@gmail.com>
2016-07-27 18:12:07 +0300

    GREY-322 Policy Comments: backend for agree/disagree functionality

--- backend/app/controllers/comments.js
+++ backend/app/controllers/comments.js
@@ -165,6 +165,10 @@ module.exports = {
             var selectFields =
                 'SELECT ' +
                 '"Comments".*, ' +
+                '(SELECT sum(CAST(CASE WHEN "c1"."isAgree" THEN 1 ELSE 0 END as INT)) as agree ' +
+                'FROM "Comments"  as c1 WHERE "c1"."parentId" = "Comments"."id"), ' +
+                '(SELECT sum(CAST(CASE WHEN "c1"."isAgree" THEN 0 ELSE 1 END as INT)) as disagree ' +
+                'FROM "Comments"  as c1 WHERE "c1"."parentId" = "Comments"."id"), ' +
                 '"Tasks"."uoaId", ' +
                 '"Tasks"."productId", ' +
                 '"SurveyQuestions"."surveyId"';
@@ -179,7 +183,7 @@ module.exports = {
                 //'INNER JOIN "Products" ON "Tasks"."productId" = "Products"."id" '+
                 'INNER JOIN "Surveys" ON "SurveyQuestions"."surveyId" = "Surveys"."id"';
 
-            var selectWhere = 'WHERE 1=1 ';
+            var selectWhere = 'WHERE "Comments"."parentId" IS NULL ';   // select only comments - not answers
             selectWhere = setWhereInt(selectWhere, req.query.questionId, 'Comments', 'questionId');
             //selectWhere = setWhereInt(selectWhere, req.query.userId, 'Comments', 'userId');
             selectWhere = setWhereInt(selectWhere, req.query.userFromId, 'Comments', 'userFromId');
@@ -225,6 +229,46 @@ module.exports = {
             }
 
             var selectQuery = selectFields + selectFrom + selectWhere + selectOrder;
+            return yield thunkQuery(selectQuery);
+        }).then(function (data) {
+            res.json(data);
+        }, function (err) {
+            next(err);
+        });
+    },
+
+    selectAnswers: function (req, res, next) {
+        var thunkQuery = req.thunkQuery;
+        co(function* () {
+            var commentId = yield * checkOneId(req, req.params.commentId, Comment, 'id', 'commentId', 'Comment');
+            var selectQuery = 'SELECT "Comments".* FROM "Comments" ';
+
+            var selectWhere = pgEscape('WHERE "Comments"."parentId" = %s ', commentId);   // select answers
+
+            //return only activated comments and draft comments for current user
+            selectWhere = selectWhere + pgEscape(' AND ("Comments"."activated" = true OR "Comments"."userFromId" = %s ) ', req.user.id);
+
+            if (!(req.query.hidden === 'true')) {
+                // show only unhidden comments
+                selectWhere = selectWhere + ' AND ("Comments"."isHidden" = false) ';
+            } else if(!auth.checkAdmin(req.user)) {
+                // specified hidden parameters for ordinary user show only self comments (include hidden)
+                selectWhere = selectWhere + pgEscape(' AND ("Comments"."isHidden" = false OR "Comments"."userFromId" = %s ) ', req.user.id);
+            } // if admin - show all hidden comments
+
+            var selectOrder = '';
+            if (req.query.order) {
+                var sorted = req.query.order.split(',');
+                for (var i = 0; i < sorted.length; i++) {
+                    var sort = sorted[i];
+                    selectOrder =
+                        ((selectOrder === '') ? 'ORDER BY ' : selectOrder + ', ') +
+                        sort.replace('-', '').trim() +
+                        (sort.indexOf('-') === 0 ? ' desc' : ' asc');
+                }
+            }
+
+            selectQuery = selectQuery + selectWhere + selectOrder;
             return yield thunkQuery(selectQuery);
         }).then(function (data) {
             res.json(data);
@@ -280,6 +324,60 @@ module.exports = {
                 object: 'Comments',
                 entity: result[0].id,
                 info: 'Add comment'
+            });
+
+            return _.first(result);
+
+        }).then(function (data) {
+            res.status(201).json(data);
+        }, function (err) {
+            next(err);
+        });
+    },
+
+    insertAnswer: function (req, res, next) {
+        var thunkQuery = req.thunkQuery;
+        co(function* () {
+            yield * checkAnswerInsert(req);
+            var parentComment = yield * common.getEntity(req, req.params.commentId, Comment, 'id');
+            req.body = _.extend(req.body,_.pick(parentComment, Comment.answerFromParentCols)); // add key values from parent comment
+            req.body = _.extend(req.body, {
+                parentId: parentComment.id
+            });
+            req.body = _.extend(req.body, {
+                userFromId: req.user.realmUserId
+            }); // add from realmUserId instead of user id
+            if (!req.query.autosave) { // if autosave is exist for answers
+                req.body = _.extend(req.body, {
+                    activated: true
+                }); // answers is activated
+            }
+            req.body = _.extend(req.body, {
+                tags: JSON.stringify(req.body.tags)
+            }); // stringify tags
+            req.body = _.extend(req.body, {
+                range: JSON.stringify(req.body.range)
+            }); // stringify range
+            // get next order for entry
+            var nextOrder = yield * getNextAnswerOrder(req, parentComment.id);
+            req.body = _.extend(req.body, {
+                order: nextOrder
+            }); // add nextOrder (if order was presented in body replace it)
+
+            req.body = _.pick(req.body, Comment.insertCols); // insert only columns that may be inserted
+            var result = yield thunkQuery(Comment.insert(req.body).returning(Comment.id));
+
+            if (req.body.activated) {
+                notify(req, result[0].id, parentComment.taskId, 'Answer added', 'Comments', 'comment');
+            }
+
+            bologger.log({
+                req: req,
+                user: req.user,
+                action: 'insert',
+                object: 'Comments',
+                entity: result[0].id,
+                info: 'Add answer to comment'
             });
 
             return _.first(result);
@@ -472,6 +570,14 @@ function* checkInsert(req) {
         //returnObject = yield * checkForReturnAndResolve(req, req.user, taskId, req.body.stepId, 'resolve');
         req.body = _.omit(req.body, 'isReturn'); // remove isReturn flag from body
     }
+}
+
+function* checkAnswerInsert(req) {
+    var commentId = yield * checkOneId(req, req.params.commentId, Comment, 'id', 'commentId', 'Comment');
+    if (!req.body.isAgree ) { // disagree
+        yield * checkString(req.body.entry, 'Entry');
+    }
+
 }
 
 function* checkUpdate(req) {
@@ -787,6 +893,24 @@ function* getNextOrder(req, taskId, questionId) {
     result = yield thunkQuery(query);
     // get next order
     // if not found records, nextOrder must be 1  - the first comment for question
+    return (!_.first(result)) ? 1 : result[0].maxorder + 1;
+}
+
+function* getNextAnswerOrder(req, commentId) {
+
+    // then get max order for answers
+    var result;
+    var query =
+        'SELECT ' +
+        'max("Comments".order) as maxorder ' +
+        'FROM ' +
+        '"Comments" ' +
+        'WHERE  ' +
+        pgEscape('"Comments"."parentId" = %s', commentId);
+    var thunkQuery = req.thunkQuery;
+    result = yield thunkQuery(query);
+    // get next order
+    // if not found records, nextOrder must be 1  - the first answer for comment
     return (!_.first(result)) ? 1 : result[0].maxorder + 1;
 }
 
