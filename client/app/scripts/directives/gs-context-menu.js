cf33c2b663a99283dc9a565ba1e2c58db2ac6e88
Igor Ivanov <iivanov@sinergo.ru>
2016-07-27 18:24:57 +0700

    re-factired comment popup menu position in sophisticated cases

--- client/app/scripts/directives/gs-context-menu.js
+++ client/app/scripts/directives/gs-context-menu.js
@@ -4,7 +4,7 @@
 'use strict';
 
 angular.module('greyscaleApp')
-    .directive('gsContextMenu', function (greyscaleSelection, $timeout, $log) {
+    .directive('gsContextMenu', function (greyscaleSelection, $timeout, $window) {
         return {
             restrict: 'A',
             transclude: true,
@@ -34,46 +34,39 @@ angular.module('greyscaleApp')
                 elem.on('contextmenu', _showContextMenu);
 
                 function _showContextMenu(evt) {
-                    var node, x, y, menu, bottom, right,
-                        limit = 1000;
+                    var menu, bottom, right,
+                        _offset;
 
                     if (_hasSelection()) {
                         evt.preventDefault();
-                        node = evt.target;
-                        x = evt.offsetX;
-                        y = evt.offsetY;
+                        menu = elem.find('#' + scope.model.menuId);
 
-                        while (node && limit && node !== evt.currentTarget) {
-                            limit--;
-                            if (node.nodeName !== 'TR') {
-                                x += node.offsetLeft;
-                                y += node.offsetTop;
-                            }
-                            node = node.offsetParent || node.parentNode;
-                        }
-                        scope.model.data = {
-                            range: window.getSelection().getRangeAt(0),
-                            selection: greyscaleSelection.get(document.getElementById(scope.qid)),
-                            selectedHtml: greyscaleSelection.html()
+                        _offset = _getOffset(evt.currentTarget);
+
+                        _offset = {
+                            left: evt.pageX - _offset.left,
+                            top: evt.pageY - _offset.top
                         };
 
-                        menu = elem.find('#' + scope.model.menuId);
                         right = evt.clientX + menu.outerWidth() + 5;
 
-                        if (x + menu.width > window.innerWidth) {
-                            x -= (right - window.innerWidth);
+                        if (right > $window.innerWidth) {
+                            _offset.left -= (right - $window.innerWidth);
                         }
 
                         bottom = evt.clientY + menu.outerHeight() + 5;
 
-                        if (bottom > window.innerHeight) {
-                            y -= (bottom - window.innerHeight);
+                        if (bottom > $window.innerHeight) {
+                            _offset.top -= (bottom - $window.innerHeight);
                         }
 
-                        menu.css({
-                            left: x,
-                            top: y
-                        });
+                        menu.css(_offset);
+
+                        scope.model.data = {
+                            range: $window.getSelection().getRangeAt(0),
+                            selection: greyscaleSelection.get($window.document.getElementById(scope.qid)),
+                            selectedHtml: greyscaleSelection.html()
+                        };
 
                         $timeout(function () {
                             elem.addClass('open');
@@ -81,8 +74,48 @@ angular.module('greyscaleApp')
                     }
                 }
 
+                function _getOffset(elem) {
+                    if (elem.getBoundingClientRect) {
+                        return getOffsetRect(elem);
+                    } else {
+                        return getOffsetSum(elem);
+                    }
+                }
+
+                function getOffsetSum(elem) {
+                    var top = 0,
+                        left = 0;
+
+                    while (elem) {
+                        top = top + parseInt(elem.offsetTop);
+                        left = left + parseInt(elem.offsetLeft);
+                        elem = elem.offsetParent;
+                    }
+
+                    return {
+                        top: top,
+                        left: left
+                    };
+                }
+
+                function getOffsetRect(elem) {
+                    var box = elem.getBoundingClientRect(),
+                        body = $window.document.body,
+                        docElem = $window.document.documentElement;
+
+                    var scrollTop = $window.pageYOffset || docElem.scrollTop || body.scrollTop,
+                        scrollLeft = $window.pageXOffset || docElem.scrollLeft || body.scrollLeft,
+                        clientTop = docElem.clientTop || body.clientTop || 0,
+                        clientLeft = docElem.clientLeft || body.clientLeft || 0;
+
+                    return {
+                        top: Math.round(box.top + scrollTop - clientTop),
+                        left: Math.round(box.left + scrollLeft - clientLeft)
+                    };
+                }
+
                 function _hasSelection() {
-                    var _range = window.getSelection().getRangeAt(0).cloneRange().toString();
+                    var _range = $window.getSelection().getRangeAt(0).cloneRange().toString();
                     return (_range && _range.length > 0);
                 }
 
