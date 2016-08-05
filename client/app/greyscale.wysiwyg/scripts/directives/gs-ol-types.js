80529035f2f05aff9f608bfc68b36dee12cae333
Igor Ivanov <iivanov@sinergo.ru>
2016-08-05 19:15:38 +0700

    implemented OL in wysiwyg with selectable list type

--- client/app/greyscale.wysiwyg/scripts/directives/gs-ol-types.js
+++ client/app/greyscale.wysiwyg/scripts/directives/gs-ol-types.js
@@ -2,52 +2,9 @@
  * Created by igi on 04.08.16.
  */
 'use strict';
-angular.module('greyscale.wysiwyg')
-    .directive('gsOlTypes', function () {
-            var olTypes = [{
-                type: '1',
-                title: '1,2,3...',
-                active: true,
-                hint: 'indicates numbers (default)'
-            }, {
-                type: 'a',
-                title: 'a,b,c...',
-                active: false,
-                hint: 'indicates lowercase letters'
-            }, {
-                type: 'A',
-                title: 'A,B,C...',
-                hint: 'indicates uppercase letters'
-            }, {
-                type: 'i',
-                title: 'i,ii,iii...',
-                active: false,
-                hint: 'indicates lowercase Roman numerals'
-            }, {
-                type: 'I',
-                title: 'I,II,III...',
-                active: false,
-                hint: 'indicates uppercase Roman numerals'
-            }];
-
-            return {
-/*                scope: {},*/
-                restrict: 'E',
-                transclude: true,
-                templateUrl: 'greyscale.wysiwyg/views/gs-ol-types.html'
-                /*
-                controller: function ($scope) {
-                    angular.extend($scope, {
-                        view: olTypes,
-                        model: olTypes[0]
-                    });
-                    $scope.select = _selectItem;
-
-                    function _selectItem(idx) {
-                        $scope.model = olTypes[idx];
-                    }
-                }
-                */
-            };
-        }
-    );
+angular.module('greyscale.wysiwyg').directive('gsOlTypes', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'greyscale.wysiwyg/views/gs-ol-types.html'
+    };
+});
--- client/app/greyscale.wysiwyg/scripts/greyscale.wysiwyg.js
+++ client/app/greyscale.wysiwyg/scripts/greyscale.wysiwyg.js
@@ -4,85 +4,132 @@
 'use strict';
 angular.module('greyscale.wysiwyg', ['textAngular', 'ui.bootstrap'])
     .config(function ($provide) {
-        $provide.decorator('taOptions', ['taRegisterTool', '$delegate', function (taRegisterTool, taOptions) {
-            /*
-             toolbar: [
-             ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre', 'quote'],
-             ['bold', 'italics', 'underline', 'strikeThrough', 'ul', 'ol', 'redo', 'undo', 'clear'],
-             ['justifyLeft','justifyCenter','justifyRight','justifyFull','indent','outdent'],
-             ['html', 'insertImage', 'insertLink', 'insertVideo', 'wordcount', 'charcount']
-             ],
-             */
-            taOptions.toolbar = [
-                ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre', 'quote'],
-                ['bold', 'italics', 'underline', 'strikeThrough', 'ul', 'ol', 'redo', 'undo', 'clear']
-            ];
-
-            taRegisterTool('colour', {
-                iconclass: 'fa fa-square text-danger',
-                tooltiptext: 'Text colour',
-                action: function () {
-                    this.$editor().wrapSelection('forecolor', 'red');
-                }
-            });
-
-            taRegisterTool('olType', {
-                display: '<span class="bar-btn-dropdown dropdown">{{getActive().title}}' +
-                '<button class="btn btn-default dropdown-toggle" type="button" ng-disabled="isDisabled()">' +
-                '<i class="fa fa-caret-down"></i></button><ul class="dropdown-menu">' +
-                '<li ng-repeat="o in options"><button class="checked-dropdown" type="button" ng-click="setType(o)">' +
-                '<i ng-if="o.active" class="fa fa-check"></i>{{o.title}}</button></li></ul></span>',
-                options: [{
-                    type: '1',
-                    title: '1,2,3...',
-                    active: true,
-                    hint: 'indicates numbers (default)'
-                }, {
-                    type: 'a',
-                    title: 'a,b,c...',
-                    hint: 'indicates lowercase letters'
-                }, {
-                    type: 'A',
-                    title: 'A,B,C...',
-                    hint: 'indicates uppercase letters'
-                }, {
-                    type: 'i',
-                    title: 'i,ii,iii...',
-                    hint: 'indicates lowercase Roman numerals'
-                }, {
-                    type: 'I',
-                    title: 'I,II,III...',
-                    hint: 'indicates uppercase Roman numerals'
-                }],
-                action: function (defer) {
-                    console.log('action fired', defer);
-                },
-                setType: function (type) {
-                    this.type.active = false;
-                    type.active = true;
-                    console.log('setType fired', type);
-                },
-                getActive: function() {
-                    var o,
-                        qty = this.options.length;
-                    this.type = this.options[0];
-
-                    for (o = 0; o < qty; o++) {
-                        if (this.options[o].active) {
-                            this.type = this.options[o];
-                        }
+        $provide.decorator('taOptions', ['taRegisterTool', 'taSelection', '_', '$delegate',
+            function (taRegisterTool, taSelection, _, taOptions) {
+                taRegisterTool('colour', {
+                    iconclass: 'fa fa-square text-danger',
+                    tooltiptext: 'Text colour',
+                    action: function () {
+                        this.$editor().wrapSelection('forecolor', 'red');
                     }
-                    return this.type;
-                },
-                activeState: function () {
-                    console.log('activeState fired');
-                    this.type = this.getActive();
-                    return false;
-                }
-            });
+                });
 
-            taOptions.toolbar[0].push('colour', 'olType');
-            return taOptions;
+                taRegisterTool('olType', {
+                    display: '<gs-ol-types class="bar-btn-dropdown dropdown"></gs-ol-types>',
+                    options: [{
+                        key: '1',
+                        title: '1,2,3...',
+                        hint: 'indicates numbers (default)'
+                    }, {
+                        key: 'a',
+                        title: 'a,b,c...',
+                        hint: 'indicates lowercase letters'
+                    }, {
+                        key: 'A',
+                        title: 'A,B,C...',
+                        hint: 'indicates uppercase letters'
+                    }, {
+                        key: 'i',
+                        title: 'i,ii,iii...',
+                        hint: 'indicates lowercase Roman numerals'
+                    }, {
+                        key: 'I',
+                        title: 'I,II,III...',
+                        hint: 'indicates uppercase Roman numerals'
+                    }],
+                    tooltiptext: 'Ordered List',
+                    action: function (def) {
+                        var _olElem,
+                            _activeType = this.getActive(),
+                            _isOl = this.$editor().queryCommandState('insertOrderedList'),
+                            _self = this;
 
-        }]);
+                        def.promise.then(__setOlType);
+
+                        if (_isOl) {
+                            _olElem = this._getOlElem(angular.element(taSelection.getSelectionElement()));
+                        }
+
+                        if (!_isOl || _olElem.type === this.getActive()) { //toggle ol
+                            _isOl = !_isOl;
+                            return this.$editor().wrapSelection('insertOrderedList', null);
+                        } else {
+                            return __setOlType();
+                        }
+
+                        function __setOlType() {
+                            if (_isOl) {
+                                _olElem = _self._getOlElem(angular.element(taSelection.getSelectionElement()));
+                                if (_olElem.elem) {
+                                    _olElem.elem.setAttribute('type', _activeType.key);
+                                }
+                            }
+                            return false;
+                        }
+                    },
+                    _getOlElem: function (elem) {
+                        var _ol, _olType;
+                        _ol = angular.element(elem).closest('ol');
+                        if (_ol.length) {
+                            _olType = _.find(this.options, {
+                                key: _ol[0].getAttribute('type') || '1'
+                            });
+                        }
+                        return {
+                            elem: _ol[0],
+                            type: _olType
+                        };
+                    },
+                    setType: function (type) {
+                        if (this.type) {
+                            this.type.active = false;
+                        }
+                        type.active = true;
+                        this.tooltiptext = type.hint;
+                    },
+                    getActive: function () {
+                        var o,
+                            qty = this.options.length;
+                        this.type = this.options[0];
+
+                        for (o = 0; o < qty; o++) {
+                            if (this.options[o].active) {
+                                this.type = this.options[o];
+                            }
+                        }
+                        return this.type;
+                    },
+                    activeState: function (commonElement) {
+                        var res = this.$editor().queryCommandState('insertOrderedList');
+
+                        if (res) {
+                            var _ol = this._getOlElem(commonElement);
+                            if (_ol.type) {
+                                this.setType(_ol.type);
+                            }
+                            this.type = this.getActive();
+                        } else {
+                            //this.setType(this.options[0]); //reset to default type;
+                        }
+                        return res;
+                    }
+                });
+                /*
+                 toolbar: [
+                 ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre', 'quote'],
+                 ['bold', 'italics', 'underline', 'strikeThrough', 'ul', 'ol', 'redo', 'undo', 'clear'],
+                 ['justifyLeft','justifyCenter','justifyRight','justifyFull','indent','outdent'],
+                 ['html', 'insertImage', 'insertLink', 'insertVideo', 'wordcount', 'charcount']
+                 ],
+                 */
+                taOptions.toolbar = [
+                    ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
+                    ['colour', 'bold', 'italics', 'underline', 'strikeThrough'],
+                    ['ul', 'olType'],
+                    ['redo', 'undo', 'clear']
+                ];
+
+                return taOptions;
+            }
+        ]);
     });
