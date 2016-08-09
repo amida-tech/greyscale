fe09a90372f0c6de17fddc5264bd69128108ace6
alexcoach <lexa.trener@gmail.com>
2016-08-09 10:12:54 +0300

    GREY-291 brush

--- client/app/scripts/directives/gs-model-validate.js
+++ client/app/scripts/directives/gs-model-validate.js
@@ -19,107 +19,107 @@
  */
 
 angular.module('greyscaleApp')
-.directive('gsModelValidate', function (gsModelValidators) {
-    return {
-        restrict: 'A',
-        require: 'ngModel',
-        scope: {
-            validators: '=gsModelValidate'
-        },
-        link: function(scope, el, attr, ngModel) {
-            var validators = gsModelValidators.parse(scope.validators);
-            angular.forEach(validators, function(validator){
-                gsModelValidators.apply(ngModel, validator, scope.$parent.modalFormRec);
-            });
-        }
-    };
-})
-.service('gsModelValidators', function(_){
-    var _validators = {
-        email: function(val){
-            return !!String(val).match(/(.+)@(.+)\.(.+)/);
-        },
-        // check for uniqueness using storage[dict] source
-        // config.storage - container object for dicts
-        // config.dict - dictionary property
-        // config.field - field name to check values
-        // config.idField - ID field to exclude current record
-        // config.noExclude - do not exclude current record from checking (default false)
-        unique: function(config){
-            return function(val, rec){
-                var idField = config.idField || 'id';
-                return !_.find(config.storage[config.dict], function(obj){
-                    return obj[config.field] === val && (config.noExclude || obj[idField] !== rec[idField]);
-                });
-            };
-        }
-    };
-
-    var _pub = {
-        addValidator: _addValidator,
-        parse: _parse,
-        apply: _apply
-    };
-
-    function _addValidator(key, isValidFn) {
-        _validators[key] = isValidFn;
-    }
-
-    function _apply(model, validator, rec) {
-        model.$parsers.unshift(function(value){
-            var valid = _isEmpty(value) || validator.isValidFn(value, rec);
-            model.$setValidity(validator.key, valid);
-            return valid ? value : undefined;
-        });
-        model.$formatters.unshift(function(value){
-            var valid = _isEmpty(value) || validator.isValidFn(value, rec);
-            model.$setValidity(validator.key, valid);
-            return value;
-        });
-    }
-
-    function _isEmpty(value) {
-        return value === undefined || value === '';
-    }
-
-    function _parse(validatorsData) {
-        var validators = [];
-        var inArray = angular.isArray(validatorsData);
-
-        angular.forEach(validatorsData || [], function (item, name) {
-            var isObject = angular.isObject(item);
-            var isTrue = item === true;
-            var isString = typeof item === 'string';
-            var isFunction = typeof item === 'function';
-            var hasName = !inArray && typeof name === 'string';
-
-            if (inArray && isString && _validators[item]) {
-                validators.push({
-                    key: item,
-                    isValidFn: _validators[item]
-                });
-            } else if (inArray && isObject) {
-                validators = validators.concat(_parse(item));
-
-            } else if (hasName && _validators[name] && isTrue) {
-                validators.push({
-                    key: name,
-                    isValidFn: _validators[name]
-                });
-            } else if (hasName && _validators[name] && isObject) {
-                validators.push({
-                    key: name,
-                    isValidFn: _validators[name](item),
-                });
-            } else if (hasName && isFunction) {
-                validators.push({
-                    key: name,
-                    isValidFn: item
+    .directive('gsModelValidate', function (gsModelValidators) {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            scope: {
+                validators: '=gsModelValidate'
+            },
+            link: function (scope, el, attr, ngModel) {
+                var validators = gsModelValidators.parse(scope.validators);
+                angular.forEach(validators, function (validator) {
+                    gsModelValidators.apply(ngModel, validator, scope.$parent.modalFormRec);
                 });
             }
-        });
-        return validators;
-    }
+        };
+    })
+    .service('gsModelValidators', function (_) {
+        var _validators = {
+            email: function (val) {
+                return !!String(val).match(/(.+)@(.+)\.(.+)/);
+            },
+            // check for uniqueness using storage[dict] source
+            // config.storage - container object for dicts
+            // config.dict - dictionary property
+            // config.field - field name to check values
+            // config.idField - ID field to exclude current record
+            // config.noExclude - do not exclude current record from checking (default false)
+            unique: function (config) {
+                return function (val, rec) {
+                    var idField = config.idField || 'id';
+                    return !_.find(config.storage[config.dict], function (obj) {
+                        return obj[config.field] === val && (config.noExclude || obj[idField] !== rec[idField]);
+                    });
+                };
+            }
+        };
 
-    return _pub;
-});
+        var _pub = {
+            addValidator: _addValidator,
+            parse: _parse,
+            apply: _apply
+        };
+
+        function _addValidator(key, isValidFn) {
+            _validators[key] = isValidFn;
+        }
+
+        function _apply(model, validator, rec) {
+            model.$parsers.unshift(function (value) {
+                var valid = _isEmpty(value) || validator.isValidFn(value, rec);
+                model.$setValidity(validator.key, valid);
+                return valid ? value : undefined;
+            });
+            model.$formatters.unshift(function (value) {
+                var valid = _isEmpty(value) || validator.isValidFn(value, rec);
+                model.$setValidity(validator.key, valid);
+                return value;
+            });
+        }
+
+        function _isEmpty(value) {
+            return value === undefined || value === '';
+        }
+
+        function _parse(validatorsData) {
+            var validators = [];
+            var inArray = angular.isArray(validatorsData);
+
+            angular.forEach(validatorsData || [], function (item, name) {
+                var isObject = angular.isObject(item);
+                var isTrue = item === true;
+                var isString = typeof item === 'string';
+                var isFunction = typeof item === 'function';
+                var hasName = !inArray && typeof name === 'string';
+
+                if (inArray && isString && _validators[item]) {
+                    validators.push({
+                        key: item,
+                        isValidFn: _validators[item]
+                    });
+                } else if (inArray && isObject) {
+                    validators = validators.concat(_parse(item));
+
+                } else if (hasName && _validators[name] && isTrue) {
+                    validators.push({
+                        key: name,
+                        isValidFn: _validators[name]
+                    });
+                } else if (hasName && _validators[name] && isObject) {
+                    validators.push({
+                        key: name,
+                        isValidFn: _validators[name](item),
+                    });
+                } else if (hasName && isFunction) {
+                    validators.push({
+                        key: name,
+                        isValidFn: item
+                    });
+                }
+            });
+            return validators;
+        }
+
+        return _pub;
+    });
