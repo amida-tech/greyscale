0874b129e257f0a2bcdfd35b5ba239a19d02cc61
alexcoach <lexa.trener@gmail.com>
2016-08-11 13:42:13 +0300

    GREY-423 revert cache config, fix requests

--- client/app/greyscale.core/scripts/services/greyscale-rest.js
+++ client/app/greyscale.core/scripts/services/greyscale-rest.js
@@ -41,9 +41,6 @@ angular.module('greyscale.core')
                 }
 
                 RestangularConfigurer.setDefaultHeaders(aHeaders);
-                RestangularConfigurer.setDefaultHttpFields({
-                    cache: true
-                });
             });
         };
     });
