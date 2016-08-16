1e4f8bf36499897b5d5a6988bf5c22745b0e2ed0
alexcoach <lexa.trener@gmail.com>
2016-08-16 15:45:01 +0300

    GREY-167 line numbering done

--- client/app/scripts/directives/line-numbers.js
+++ client/app/scripts/directives/line-numbers.js
@@ -6,213 +6,123 @@ function paragraphNumbersDirective(_, $timeout) {
     return {
         restrict: 'CA',
         link: function(scope, el, attr){
-            // if (!el.is(':visible')) {
-            //     return;
-            // }
 
-            var lineTags = ['P', 'LI',
+            var lineTags = [
+                'P', 'LI',
                 'H1', 'H2', 'H3', 'H4', 'H5',
+                'TR'
             ];
 
             $timeout(function(){
                 if ((attr.contenteditable && attr.taBind) || el.hasClass('line-numbers')) {
-                    normalize(el[0]);
+                    _setLineNumbers(el[0]);
                     if (attr.contenteditable) {
-                        el.on('input', function(){
-                            normalize(el);
+                        el.closest('text-angular').on('input click paste cut', function(){
+                            _setLineNumbers(el[0]);
                         });
                     }
-                    // el.contents().filter(function() {
-                    //     console.log(this);
-                    //     return this.nodeType == 3;  // Only text nodes
-                    // }).wrap('<p class="numbered-line"></p>');
-                    //el.filter('br').remove();
-                    //el.html(_transform());
                 }
 
             });
 
-            function loopChildNodes(parentNode, fn) {
-                if (!parentNode.childNodes || !parentNode.childNodes.length) {
-                    return;
-                }
-                for (var i = 0; i < parentNode.childNodes.length; i++) {
-                    var node = map[i];
-                    fn(node);
-                }
-            }
-
-            function normalize(parentNode, inLine) {
-                loopChildNodes(parentNode.childNodes, function(node){
-                    if (isText(node)) {
-                        wrapNl(node);
-
-                    } else if (!inLine && isLine(node)) {
-                        node.isLine = true;
-                        normalize(parentNode, false)
+            function _setLineNumbers(parentNode, insideLine) {
+                _loopChildNodes(parentNode, function(node){
+                    if (_isEmpty(node)) {
+                        return;
+                    }
+                    var isText = _isText(node);
+                    var isLine = _isLine(node);
+                    var hasTextChild = _hasTextChild(node);
+                    var hasLineChild = _hasLineChild(node);
+                    var hasDeepLineChild = _hasDeepLineChild(node);
+                    if (isText) {
+                        if (!insideLine) {
+                            _setLn(node);
+                        }
+                    } else if (hasDeepLineChild) {
+                        _setLineNumbers(node, !hasLineChild);
+                        if (hasTextChild) {
+                            $(node).addClass('ln');
+                        }
+                    } else if (isLine && hasTextChild) {
+                        if (!insideLine) {
+                            $(node).addClass('ln');
+                        }
+                        _setLineNumbers(node, true);
                     }
                 });
-
-
-                    // var textInDeepMap = nodeSearchDeep(this, function(n){
-                    //     return n.nodeType === 3;
-                    // });
-                    //
-                    // console.log(textInDeepMap);
-
-                    // findLines(textInDeepMap);
-                    // if (this.nodeType === 3) {
-                    //     wrapNl(this);
-                    // } else if (!nodeHasTextChildren(this)) {
-                    //     normalize($(this));
-                    // } else {
-                    //     // } else if (!$this.hasClass('ln') &&
-                    //     //     this.tagName !== 'LN' &&
-                    //     //     !parentHasParentLn(this)) {
-                    //     //     console.log('>>>>', $this.text(), ' === ' , $this.text() === '');
-                    //     //     $this.addClass('ln');
-                    //     // }
-                    //     $this.addClass('ln');
-                    // }
             }
 
-            function findLines(map) {
-                for (var i = 0; i < map.length; i++) {
-                    console.log(map[i]);
-                }
+            function _isEmpty(node) {
+                return $(node).text() === '';
             }
 
-            function nodeSearchDeep(node, criteria) {
-                var match;
-                var subMatches = [];
-                var c = node.firstChild;
-                do {
-                    if (criteria(c)) {
-                        match = c;
-                    } else {
-                        if (c.childNodes.length) {
-                            var subMatch = nodeSearchDeep(c, criteria);
-                            if ((subMatch && subMatch.nodeType) || (subMatch && subMatch.length)) {
-                                subMatches.push(subMatch);
-                            }
-                        }
-                    }
-                } while (!match && (c = c.nextSibling));
-                return match || (subMatches.length ? subMatches : null);
+            function _isText(node) {
+                return node.nodeType === 3 || (!_isLine(node) && $(node).text() === $(node).html());
             }
 
-            function isText(node) {
-                return node.nodeType === 3 || node.textContent;
+            function _isLine(node) {
+                return lineTags.indexOf(node.tagName) >= 0;
             }
 
-            function isLine(node) {
-                return lineTags.indexOf(node.tagName) >= 0 && node.childNodes.length;
-            }
-
-            /*
-            .. search text/line(tag & children)
-            .... is text -> wrap p.ln
-            text
-            .... is line -> search text/line
-            <p>
-              ....  line search, is text -> wrap ln
-              text
-              .... no text no line-> search text/line
-              <ul>
-                .... is line -> search text/line
-                <li>
-                  .... is text -> wrap p.ln
-                  text
-                </li>
-                .... no text no line -> search text/line
-                <li>
-                  .... no text no line -> search text/line
-                  <ul>
-                    .... no text no line -> search text/line
-                    <li>text</li>                 -> wrap
-                    <li>text <b>text</b></li>
-                  </ul>
-                </li>
-              </ul>
-            </p>
-            */
-
-            /*
-
-
-            */
-
-            function getDeepTextParent(node) {
-                var next = node.firstChild;
-                var textParent;
-                while (!textParent && next) {
-                    if (nodeHasTextChildren(next)) {
-                        textParent = next;
-                    } else {
-
-                    }
-                }
-                return textParent;
-            }
-
-            function nodeHasTextChildren(node) {
+            function _hasDeepLineChild(node) {
                 var has = false;
-                var c = node.firstChild;
-                while (!has && c) {
-                    if (c.nodeType === 3) {
+                _loopChildNodes(node, function(n) {
+                    if (!has && (n.tagName === 'BR' || (_isLine(n) || _hasDeepLineChild(n)))) {
                         has = true;
                     }
-                    c = c.nextSibling;
-                }
+                    if (n.tagName === 'BR') {
+                        n.remove();
+                    }
+
+                });
                 return has;
             }
 
-            function wrapNl(node) {
-                if (node.textContent) {
-                    node.className += ' nl';
-                } else {
-                    var parent = this.parentNode;
-                    var wrapper = document.createElement('ln');
+            function _hasLineChild(node) {
+                var has = false;
+                _loopChildNodes(node, function(n) {
+                    if (!has && (n.tagName === 'BR' || _isLine(n))) {
+                        has = true;
+                    }
+                    if (n.tagName === 'BR') {
+                        n.remove();
+                    }
+
+                });
+                return has;
+            }
+
+            function _hasTextChild(node) {
+                var has = false;
+                _loopChildNodes(node, function(n){
+                    if (!has && (_isText(n))) {
+                        has = true;
+                    }
+                });
+                return has;
+            }
+
+            function _setLn(node) {
+                if (node.nodeType === 3) {
+                    var parent = node.parentNode;
+                    var wrapper = document.createElement('p');
+                    wrapper.className += 'ln';
                     parent.replaceChild(wrapper, node);
                     wrapper.appendChild(node);
+                } else if (_isLine(node)) {
+                    $(node).addClass('ln');
                 }
             }
 
-            function parentHasParentLn(node) {
-                var parentNode = node.parentNode;
-                var hasLn = false;
-                while (!hasLn && parentNode) {
-                    if (parentNode.hasLn || parentNode.tagName === 'LI' || $(parentNode).hasClass('ln')) {
-                        hasLn = true;
-                    }
-                    parentNode = parentNode.parentNode;
+            function _loopChildNodes(parentNode, fn) {
+                var nodes = parentNode.childNodes;
+                if (!nodes || !nodes.length) {
+                    return;
                 }
-                return hasLn;
-            }
-
-            function _transform(html) {
-                var transformedHtml = html;
-
-
-
-                transformedHtml = html.replace(/^/gm, function() {
-                   return '<div class="numbered-line"></div>';
-                });
-
-                transformedHtml = html.replace(/<br>/gm, function() {
-                    return '<div class="numbered-line"></div>';
-                });
-
-                // if (html) {
-                //     console.log('f');
-                //     var htmlEl = angular.element('<div></div>').html(html);
-                //     console.log(htmlEl.find('br,p'));
-                //     htmlEl.find('br').addClass('numbered-line');
-                //
-                //     transformedHtml = htmlEl.html();
-                // }
-
-                return transformedHtml;
+                for (var i = 0; i < nodes.length; i++) {
+                    fn(nodes[i]);
+                }
             }
         }
     };
