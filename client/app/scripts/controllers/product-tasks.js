70233e74c08d04243b90dd1d8c4833cb30a24864
alexcoach <lexa.trener@gmail.com>
2016-08-18 17:51:34 +0300

    GREY-411 add project workflow template id for auto applying

--- client/app/scripts/controllers/product-tasks.js
+++ client/app/scripts/controllers/product-tasks.js
@@ -824,11 +824,12 @@ angular.module('greyscaleApp')
         }
 
         var _productCached;
+
         function _loadProduct(productId) {
             if (_productCached) {
                 return $q.when(_productCached);
             } else {
-                return _productCached = greyscaleProductApi.get(productId)
+                _productCached = greyscaleProductApi.get(productId)
                     .then(function (product) {
                         $state.ext.productName = product.title;
                         return product;
@@ -837,6 +838,7 @@ angular.module('greyscaleApp')
                         greyscaleUtilsSrv.errorMsg(error, tns + 'PRODUCT_NOT_FOUND');
                         $state.go('home');
                     });
+                return _productCached;
             }
         }
 
