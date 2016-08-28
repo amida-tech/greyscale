172d631d0547f7b7d151d6972f3c718864126c44
d.tseytlin <dtseytlin@gmail.com>
2016-08-29 00:32:46 +0300

    fix patch

--- backend/db_dump/patches/20160828-01-TaskUserStates.sql
+++ backend/db_dump/patches/20160828-01-TaskUserStates.sql
@@ -5,22 +5,21 @@ DECLARE
     trigger_sql text;
     db_user constant text := 'indaba'; -- HAVE TO SET CORRECT DB USER
 BEGIN
-	FOR schema_name IN
-		SELECT pg_catalog.pg_namespace.nspname
-		FROM pg_catalog.pg_namespace
-		INNER JOIN pg_catalog.pg_user
-		ON (pg_catalog.pg_namespace.nspowner = pg_catalog.pg_user.usesysid)
-		AND (pg_catalog.pg_user.usename = db_user)
+
+    FOR schema_name IN
+        SELECT pg_catalog.pg_namespace.nspname
+        FROM pg_catalog.pg_namespace
+        INNER JOIN pg_catalog.pg_user
+        ON (pg_catalog.pg_namespace.nspowner = pg_catalog.pg_user.usesysid)
+        AND (pg_catalog.pg_user.usename = db_user)
     LOOP
         RAISE NOTICE 'db_user = %, schema = %', db_user, schema_name;
+        EXECUTE 'SET search_path TO ' || quote_ident(schema_name);
 
-        EXECUTE $query$
-            SET search_path TO $query$ || schema_name || $query$;
-
-            ALTER TABLE "TaskUserStates"
-            DROP CONSTRAINT "TaskUserStates_pkey" ,
-            ADD COLUMN "surveyVersion" integer NOT NULL DEFAULT 0,
-            ADD CONSTRAINT "TaskUserStates_pkey" PRIMARY KEY ("taskId", "userId", "surveyVersion");
+        EXECUTE 'ALTER TABLE "TaskUserStates" ' ||
+            'DROP CONSTRAINT "TaskUserStates_pkey" , ' ||
+            'ADD COLUMN "surveyVersion" integer NOT NULL DEFAULT 0, ' ||
+            'ADD CONSTRAINT "TaskUserStates_pkey" PRIMARY KEY ("taskId", "userId", "surveyVersion");';
 
     END LOOP;
 
