2a2202cf6773ebb7617962478b59cc5e00303bd3
d.tseytlin <dtseytlin@gmail.com>
2016-09-08 16:41:28 +0300

    #GREY-263 new err handler, DbError handler, DB parser (code 23503)

--- backend/app/error/index.js
+++ backend/app/error/index.js
@@ -2,14 +2,17 @@ var util = require('util'),
     http = require('http'),
     errCode = require('app/error/err_code');
 
-function HttpError(status, err) {
+var debug = require('debug')('debug_errorHandler');
+var error = require('debug')('error');
+debug.log = console.log.bind(console);
+
+function HttpError(status, err, httpStatus) {
     Error.apply(this, arguments);
     Error.captureStackTrace(this, HttpError);
     var errNumber = (err && typeof err === 'number' ? true : false);
 
-    this.status = status;
+    this.status = httpStatus ? httpStatus : 400;
     this.message = {
-        '!': 0,
         'e': (errNumber ? err : this.status),
         'message': (errNumber && errCode[err] ? errCode[err] : err || http.STATUS_CODES[status] || 'Error')
     };
@@ -20,3 +23,45 @@ util.inherits(HttpError, Error);
 HttpError.prototype.name = 'HttpError';
 
 exports.HttpError = HttpError;
+
+function DbError(err, httpStatus) {
+    Error.apply(this, arguments);
+    Error.captureStackTrace(this, DbError);
+
+    this.status = httpStatus ? httpStatus : 400;
+
+    // parse db error message
+    debug('Error message: '+err.message);
+    debug('Error details: '+err.detail);
+    var mesArr = [];
+    var key;
+
+    switch (err.code) {
+        case '23503':
+            mesArr = err.message.split('"');
+            if (mesArr && mesArr.length === 7) {
+                key = ((typeof mesArr[1] === 'string') ? mesArr[1].toUpperCase() : '') +
+                    '_HAS_' +
+                    ((typeof mesArr[5] === 'string') ? mesArr[5].toUpperCase() : '') +
+                    '_' +
+                    ((typeof mesArr[3] === 'string') ? ((typeof mesArr[3].split('_')[1] === 'string') ? mesArr[3].split('_')[1].toUpperCase() : '') : '');
+            } else {
+                key = err.message;
+            }
+            break;
+        default:
+            key = err.message;
+        }
+
+
+    this.message = {
+        'e': err.code,
+        'message': key
+    };
+}
+
+util.inherits(DbError, Error);
+
+DbError.prototype.name = 'DbError';
+
+exports.DbError = DbError;
