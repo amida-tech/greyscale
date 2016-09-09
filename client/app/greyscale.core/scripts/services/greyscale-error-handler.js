a6cdbf13da7c887d97197009a74c968026c3cc97
Igor Ivanov <iivanov@sinergo.ru>
2016-09-09 13:01:36 +0700

    pre-processing REST-errors in greyscaleErrorHandler, re-factored login-form error messages

--- client/app/greyscale.core/scripts/services/greyscale-error-handler.js
+++ client/app/greyscale.core/scripts/services/greyscale-error-handler.js
@@ -4,17 +4,25 @@
 'use strict';
 
 angular.module('greyscale.core')
-    .factory('greyscaleErrorHandler', function (greyscaleUtilsSrv, $log) {
-        var i18nPrefix = 'API_ERRORS.';
+    .factory('greyscaleErrorHandler', function (i18n, i18nData) {
+        var _errors = i18nData.translations.API_ERRORS || {};
 
         return {
             errorInterceptor: _errorInterceptor
         };
 
         function _errorInterceptor(response, deferred, handler) {
-            if (!response.data || !response.data.message) {
-                $log.debug(response);
-                greyscaleUtilsSrv.errorMsg(i18nPrefix + response.status);
+            if (response.status > 399) {
+                // set error message for standard errors, not from API
+                if (!response.data || !response.data.message) {
+                    response.data = angular.extend(response.data || {}, {
+                        message: response.status
+                    });
+                }
+                //try to translate them so catch handlers may just display it
+                if (_errors[response.data.message]) {
+                    response.data.message = i18n.translate('API_ERRORS.' + response.data.message);
+                }
             }
             return true; //error not handled
         }
