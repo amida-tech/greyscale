79511fa040a9735218665a05a3910c2fd260affd
Perry Ogwuche <perryogwuche1@gmail.com>
2017-10-19 10:26:28 -0400

    removed create survey test from discussion.integration

--- backend/test/discussion.integration.js
+++ backend/test/discussion.integration.js
@@ -116,9 +116,7 @@ describe('discussion integration', function surveyIntegration() {
         it(`invite user ${index}`, userTests.inviteUserFn(user));
         it(`user ${index} activates`, userTests.selfActivateFn(index + 1));
     });
-
-    it('create survey', surveyTests.createSurveyFn(legacy));
-
+    
     _.range(2).forEach((index) => {
         it(`create product ${index}`, productTests.createProductFn(0));
         it(`create workflow ${index}`, workflowTests.createWorkflowFn(index));
