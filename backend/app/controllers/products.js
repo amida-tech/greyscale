876485a8ccf578803903353442da9a6da627107b
jmfadeley <jmfadeley@gmail.com>
2018-01-30 13:32:47 -0500

    Change that returns a flag for the products tasks.

--- backend/app/controllers/products.js
+++ backend/app/controllers/products.js
@@ -265,33 +265,13 @@ module.exports = {
                     'SELECT ' +
                     '"Discussions"."id" ' +
                     'FROM "Discussions" ' +
-                    'WHERE "Discussions"."returnTaskId" = "Tasks"."id" ' +
-                    'AND "Discussions"."isReturn" = true ' +
+                    'WHERE "Discussions"."taskId" = "Tasks"."id" ' +
                     'AND "Discussions"."isResolve" = false ' +
-                    'AND "Discussions"."activated" = true ' +
                     'LIMIT 1' +
                     ') IS NULL ' +
                     'THEN FALSE ' +
                     'ELSE TRUE ' +
                     'END as flagged',
-                    '( ' +
-                    'SELECT count("Discussions"."id") ' +
-                    'FROM "Discussions" ' +
-                    'WHERE "Discussions"."returnTaskId" = "Tasks"."id" ' +
-                    'AND "Discussions"."isReturn" = true ' +
-                    'AND "Discussions"."isResolve" = false ' +
-                    'AND "Discussions"."activated" = true ' +
-                    ') as flaggedCount',
-                    '(' +
-                    'SELECT ' +
-                    '"Discussions"."taskId" ' +
-                    'FROM "Discussions" ' +
-                    'WHERE "Discussions"."returnTaskId" = "Tasks"."id" ' +
-                    'AND "Discussions"."isReturn" = true ' +
-                    'AND "Discussions"."isResolve" = false ' +
-                    'AND "Discussions"."activated" = true ' +
-                    'LIMIT 1' +
-                    ') as flaggedFrom',
                     'CASE ' +
                     'WHEN "' + pgEscape.string(curStepAlias) + '"."position" IS NULL AND ("WorkflowSteps"."position" = 0) THEN \'current\' ' +
                     'WHEN "' + pgEscape.string(curStepAlias) + '"."position" IS NULL AND ("WorkflowSteps"."position" <> 0) THEN \'waiting\' ' +
