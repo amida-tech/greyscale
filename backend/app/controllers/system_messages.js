1d171e96fbdfe0bd3b466ba28465b9aff327def4
Perry Ogwuche <perryogwuche1@gmail.com>
2018-02-08 15:50:30 -0500

    cleaned up code

--- backend/app/controllers/system_messages.js
+++ backend/app/controllers/system_messages.js
@@ -11,28 +11,15 @@ module.exports = {
         ) {
             res.status(400).send('System message must have a \'to\' field and a \'message\' field').end();
         } else {
-
-            console.log()
-            console.log(`TRYING TO NOTIFY BY CALLING sendSystemMessageWithMessageService()`)
-
             common.sendSystemMessageWithMessageService(req, req.body.to, req.body.message)
-            .then((res) => {
-
-                console.log(`RESPONSE FROM THEN IS: ${res}`)
+            .then((response) => {
+                if (response.statusCode === 204) {
+                    res.status(204).end();
+                } else {
+                    res.status(400, res.message).end();
+                    throw new Error(res.message);
+                }
             })
-
         }
-
-
-        // console.log(`RESPONSE FROM SEND IS ${response.statusCode}`)
-        //
-        // if (response.statusCode === 204) {
-        //     console.log(`I GOT IN HERE AFTERALL`)
-        //     result = response;
-        //     res.status(204).end();
-        // } else {
-        //     res.status(400, response.message).end();
-        //     throw new Error(response.message);
-        // }
     }
 }
