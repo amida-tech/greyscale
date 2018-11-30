a27d2bf41f7e3dc4b285c435d7d987cde8bee827
jmfadeley@gmail.com <jmfadeley@gmail.com>
2018-11-30 17:28:51 -0500

    [DEVOPS-365] Much progress. Stepped up to a higher level of abstraction to make calls.

--- backend/newseed.js
+++ backend/newseed.js
@@ -4,8 +4,10 @@ const _ = require('lodash');
 const config = require('./config');
 const models = require('./models');
 const request = require('request-promise');
+const jwt = require('jsonwebtoken');
 const schema = _.get(config, 'testEntities.organization.realm', 'public');
 const db = models(config.pgConnect, ['sceleton', 'test']);
+const appGenerator = require('./app/app-generator');
 
 const superAdmin = config.testEntities.superAdmin;
 const admin = config.testEntities.admin;
@@ -15,49 +17,61 @@ let activeToken = null;
 let addToAuth = true;
 
 // Add '--blank' to create database without test users.
-function loginAuthAdmin() {
-    const requestOptions = {
-        url: config.authService + '/auth/login',
+function requestGenerator() {
+    return ({
         method: 'POST',
         headers: {
             'origin': config.domain
         },
-        json: {
-            username: process.env.AUTH_SERVICE_SEED_ADMIN_USERNAME,
-            email: process.env.AUTH_SERVICE_SEED_ADMIN_USERNAME,
-            password: process.env.AUTH_SERVICE_SEED_ADMIN_PASSWORD,
-        },
         resolveWithFullResponse: true,
-    };
+    });
+};
+
+function requestCall(requestOptions, operation) {
     return request(requestOptions)
         .then((res) => {
-            if (res.statusCode > 299 || res.statusCode < 200) {
-                const httpErr = new HttpError(res.statusCode, res.statusMessage);
-                console.log('*********\nAdmin authentication failed. Check your username and password, or .env settings.\n');
-                return Promise.reject(httpErr);
+            if (operation === 'login') {
+                activeToken = 'Bearer ' + res.body.token;
             }
-            activeToken = 'Bearer ' + res.body.token;
             return Promise.resolve();
         })
         .catch((err) => {
-            console.log('*********\nContacting the auth service failed. Check if it\'s up.\n');
-            return Promise.reject(httpErr);
+            if (err.statusCode === 409 && operation === 'create user') {
+                console.log('*********\nUser ' + requestOptions.json.username + ' already in auth service.\n');
+                return Promise.resolve();
+            }
+            console.log('*********\nFailed to connect with one of the services.\n');
+            return Promise.reject(err);
         });
 }
 
+function loginAuth(email, password) {
+    const requestOptions = requestGenerator();
+    requestOptions.url = config.authService + '/auth/login';
+    requestOptions.json = {
+        username: email,
+        email,
+        password,
+    };
+    return requestCall(requestOptions, 'login');
+}
+
+function mockAuth(email, scopes) {
+    const payload = {
+        username: email,
+        email,
+        scopes: [scopes],
+    };
+    activeToken = 'Bearer ' + jwt.sign(payload, config.jwtSecret);
+}
+
 function createUserOnAuth(email, password, scopes) {
-    const requestOptions = {
-        url: config.authService + '/user',
-        method: 'POST',
-        headers: {
-            'origin': config.domain
-        },
-        json: {
-            username: email,
-            email,
-            password,
-        },
-        resolveWithFullResponse: true,
+    const requestOptions = requestGenerator();
+    requestOptions.url = config.authService + '/user';
+    requestOptions.json = {
+        username: email,
+        email,
+        password,
     };
     if (scopes) {
         requestOptions.json.scopes = [scopes];
@@ -65,27 +79,14 @@ function createUserOnAuth(email, password, scopes) {
     if (activeToken) {
         requestOptions.headers.authorization = activeToken;
     }
-    return request(requestOptions)
-        .then((res) => {
-            if (res.statusCode > 299 || res.statusCode < 200) {
-                const httpErr = new HttpError(res.statusCode, res.statusMessage);
-                console.log('*********\nInsertion failed for ' + email + '.\n');
-                return Promise.reject(httpErr);
-            }
-            return Promise.resolve();
-        })
-        .catch((err) => {
-            console.log('*********\nContacting the auth service failed. Check if it\'s up.\n');
-            return Promise.reject(httpErr);
-        });
+    return requestCall(requestOptions, 'create user');
 }
 
 db.sequelize.query(`SELECT COUNT(*) AS count FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'Users'`, { type: db.sequelize.QueryTypes.SELECT })
     .then((result) => {
         if(result[0].count === '0') {
             console.log('*********\nDatabase empty. Proceeding with initialization.');
-            console.log('Procedure will ' +(process.argv.includes('--blank') ? 'not ' : '')
-                + 'create test users.\n');
+            console.log('Procedure will ' +(process.argv.includes('--blank') ? 'not ' : '') + 'create test users and organization.\n');
             return db.sequelize.sync({ force: true });
         }
         console.log('*********\nThe tables are already initialized. If this is an error, please drop and recreate the database.');
@@ -93,20 +94,19 @@ db.sequelize.query(`SELECT COUNT(*) AS count FROM information_schema.tables WHER
     })
     .then(() => {
         if (process.argv.includes('--blank')) {
-            addToAuth = false;
-            return null;
+            console.log('*********\nSeeding process complete.');
+            process.exit(0);
         }
         if (process.env.AUTH_SERVICE_SEED_ADMIN_PASSWORD) {
             console.log('*********\nAdmin password provided. Using to create users on auth service.\n');
-            return loginAuthAdmin();
+            return loginAuth(process.env.AUTH_SERVICE_SEED_ADMIN_USERNAME, process.env.AUTH_SERVICE_SEED_ADMIN_PASSWORD);
         } else if (process.env.AUTH_SERVICE_PUBLIC_REGISTRATION) {
             console.log('*********\nPublic registration true. Creating users on auth service.\n');
         } else {
             console.log('*********\nNo means to create users on auth service detected. Only creating them in Indaba.\n');
-            addToAuth = false;
         }
     })
-    .then(() => {
+    .then(() => { // Add users to auth if available.
         if(addToAuth) {
             const promiseChain = [];
             promiseChain.push(createUserOnAuth(superAdmin.email, superAdmin.password, superAdmin.scopes));
@@ -114,9 +114,41 @@ db.sequelize.query(`SELECT COUNT(*) AS count FROM information_schema.tables WHER
             users.forEach((user) => {
                 promiseChain.push(createUserOnAuth(user.email, user.password));
             });
+            promiseChain.push(createUserOnAuth(config.systemMessageUser, config.systemMessagePassword));
             return Promise.all(promiseChain);
         }
         return null;
+    })
+    .then(() => { // Start the application.
+        return appGenerator.generate();
+    })
+    .then(() => { // Create organization.
+        mockAuth(superAdmin.email, superAdmin.scopes);
+        const requestOptions = requestGenerator();
+        requestOptions.url = config.domain + '/public/v0.2/organizations';
+        requestOptions.headers.authorization = activeToken;
+        requestOptions.json = {
+            realm: organization.realm,
+        };
+        console.log(requestOptions);
+        return requestCall(requestOptions, 'create organization');
+    })
+    .then(() => { // Set search_path to new organization
+        return db.sequelize.query('SET search_path TO ' + organization.realm);
+    })
+    .then(() => { // Invite admin.
+        const requestOptions = requestGenerator();
+        requestOptions.url = config.domain + '/' + organization.realm + '/v0.2/users/self/organization/invite';
+        requestOptions.headers.authorization = activeToken;
+        requestOptions.json = admin;
+        return requestCall(requestOptions, 'create admin');
+    })
+    .then(() => { // Login as admin.
+        if(!addToAuth) {
+            mockAuth(admin.email, admin.scopes);
+            return null;
+        }
+        return loginAuth(admin.email, admin.scopes);
     })
     .then(() => {
         console.log('*********\nSeeding process complete.');
